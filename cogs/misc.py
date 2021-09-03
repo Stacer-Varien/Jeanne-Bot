facd1ab7641a1d03ccc2b99a186cabf6233ecaeb
ZaneRE544 <82768608+ZaneRE544@users.noreply.github.com>
2021-09-03 12:32:37 +0200

    Added more invite links

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,11 +10,23 @@ class misc(commands.Cog):
     async def invite(self, ctx):
         embed = discord.Embed(
             title="Invite me!",
-            url="https://discord.com/oauth2/authorize?client_id=831993597166747679&scope=bot&permissions=469888182",
+            description="Click on one of these URLs to invite me to you server or join my creator's servers",
             color=0x00bfff)
-        embed.add_field(name="Or if you want to join the support server:",
-                        value="https://discord.gg/Xn3EvGcMrF",
+        embed.add_field(name="Bot Invite",
+                        value="[Click here](https://discord.com/oauth2/authorize?client_id=831993597166747679&scope=bot&permissions=469888182)",
                         inline=True)
+        embed.add_field(name="Top.gg",
+                        value="[Click here](https://top.gg/bot/831993597166747679)",
+                        inline=True)
+        embed.add_field(name="Top.gg",
+                        value="[Click here](https://top.gg/bot/831993597166747679)",
+                        inline=True)
+        embed.add_field(name="DiscordBotList",
+                        value="[Click here](https://discordbotlist.com/bots/nero-3694)", inline=True)
+        embed.add_field(name="HAZE server",
+                        value="[Click here](https://discord.gg/VVxGUmqQhF)", inline=True)
+        embed.add_field(name="NERO Support Server",
+                        value="[Click here](https://discord.gg/Xn3EvGcMrF)", inline=True)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -24,7 +36,7 @@ class misc(commands.Cog):
         await message.delete()
         await ctx.send(text)
 
-    @commands.group(aliases=['saye'])
+    @commands.group(name='saye')
     @commands.has_permissions(administrator=True)
     async def sayembed(self, ctx, *, text):
         message = ctx.message
