c339c075f5fd960fc97d28bf895a4b8e508434a9
ZaneRE544 <82768608+ZaneRE544@users.noreply.github.com>
2021-09-03 12:31:46 +0200

    Removed av and added flip help

--- cogs/help.py
+++ cogs/help.py
@@ -14,18 +14,18 @@ class help(commands.Cog):
                               description="Here are some commands to help you.",
                               color=0x236ce1)
         embed.add_field(name="Fun",
-                        value="8 Ball (8b, 8ball)\nHentai (h) **NSFW**\nRoll Dice (rd, dice)\nCombine\nAnimeme (meme, animememe)",
+                        value="8 Ball (8b, 8ball)\nHentai (h) **NSFW**\nRoll Dice (rd, dice)\nCombine\nAnimeme (meme, animememe)\nFlip (coinflip, headsortails, piece)",
                         inline=True)
         embed.add_field(
             name="Info",
-            value="User Info (uinfo, minfo, guild, guildinfo)\nServer Info (sinfo)\nAvavtar (av)",
+            value="User Info (uinfo, minfo, guild, guildinfo)\nServer Info (sinfo)\nPing\nStats",
             inline=True)
         embed.add_field(
             name="Creator Only",
             value="Find Server (fserver)\nFind User (fuser)\nMutuals\nBot Mutuals (bm)",
             inline=True)
         embed.add_field(
-            name="Misc", value="Ping\nHelp\nInvite\nSay\nSayembed (saye)", inline=True)
+            name="Misc", value="Help\nInvite\nSay\nSayembed (saye)", inline=True)
         embed.add_field(name="Moderation",
                         value="Warn (w)\nKick (k)\nBan (b)\nUnban (unb)\nPurge\nMute\nUnmute",
                         inline=True)
@@ -35,16 +35,17 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command()
-    async def ping(self, ctx, *arg):
+    async def ping(self, ctx):
         embed = discord.Embed(title="Ping Help",
                               description="Check how fast I respond to a command",
                               color=0x236ce1)
         embed.add_field(name="Example",
                         value="re!ping",
                         inline=True)
+        await ctx.send(embed=embed)
 
     @help.command(aliases=['8b', '8ball'])
-    async def _8ball(self, ctx, *arg):
+    async def _8ball(self, ctx):
         embed = discord.Embed(
             title="8 ball help",
             description="Ask 8 ball anything and you will get your awnser\nAliases: 8b, 8ball",
@@ -54,7 +55,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['uinfo', 'minfo'])
-    async def userinfo(ctx, *arg):
+    async def userinfo(self, ctx):
         embed = discord.Embed(
             title="Serverinfo help",
             description="See the information of a member or yourself\nAliases: uinfo, userinfo, minfo\n\n**NOTE:** It must be someone present in the server.",
@@ -66,7 +67,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['sinfo', 'guild', 'guildinfo', 'ginfo'])
-    async def serverinfo(self, ctx, *arg):
+    async def serverinfo(self, ctx):
         embed = discord.Embed(
             title="Serverinfo help",
             description="Get information about this server\nAliases: serverinfo, sinfo, guild, guildinfo, ginfo",
@@ -75,7 +76,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['fuser'])
-    async def finduser(self, ctx, *arg):
+    async def finduser(self, ctx):
         embed = discord.Embed(
             title="Finduser help",
             description="Finds a user in Discord\nAliases: finduser, fuser\n\n**NOTE**: This is an ***OWNER ONLY*** command which means only my creator can use it",
@@ -84,7 +85,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['fserver'])
-    async def findserver(self, ctx, *arg):
+    async def findserver(self, ctx):
         embed = discord.Embed(
             title="Findserver help",
             description="Finds a server I'm mutual with\nAliases: findserver, fserver\n\n**NOTE**: This is an ***OWNER ONLY*** command which means only my creator can use it",
@@ -93,20 +94,8 @@ class help(commands.Cog):
                         value="re!fserver SERVER", inline=False)
         await ctx.send(embed=embed)
 
-    @help.command(aliases=['av'])
-    async def avatar(self, ctx, *arg):
-        embed = discord.Embed(
-            title="Avatar help",
-            description="See the profile picture of a member or yourself\nAliases: avatar, av\n\n**NOTE**: It must be a member present in the server",
-            color=0x093cb3)
-        embed.add_field(
-            name="Example:",
-            value="re!av (if yourself)\nre!av MEMBER (if for a member)",
-            inline=False)
-        await ctx.send(embed=embed)
-
     @help.command()
-    async def say(self, ctx, *arg):
+    async def say(self, ctx):
         embed = discord.Embed(
             title="Say help",
             description="Type a message and I will say it but it will be in plain text.\n\n**Required Permission:** Administrator\n\n**NOTE:** After typing the message, your message will be deleted but said by me. The message will be plain text.",
@@ -115,7 +104,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(alises=['saye'])
-    async def sayembed(self, ctx, *arg):
+    async def sayembed(self, ctx):
         embed = discord.Embed(
             title="Sayembed help",
             description="Type a message and I will say it but it will be in embed.\n\n**Required Permission:** Administrator\n\n**NOTE:** After typing the message, your message will be deleted but said by me",
@@ -137,7 +126,7 @@ class help(commands.Cog):
     async def purge(self, ctx):
         embed = discord.Embed(
             title="Purge help",
-            description="Bulk delete messages.\n\n**Required Permission:** Manage Messages\n\n**NOTE:** Will delete up to 50 messages. You can also mention a member or add a number less than 50 to delete the messages",
+            description="Bulk delete messages.\n\n**Required Permission:** Manage Messages\n\n**NOTE:** Will delete up to 100 messages. You put a number less than 100 to delete the messages",
             color=0x093cb3)
         embed.add_field(
             name="Example:", value="re!purge 20", inline=False)
@@ -162,11 +151,11 @@ class help(commands.Cog):
         embed.add_field(
             name="Example",
             value="re!h",
-            inline=False)
+            inline=True)
         await ctx.send(embed=embed)
 
     @help.command()
-    async def warn(self, ctx, *arg):
+    async def warn(self, ctx):
         embed = discord.Embed(
             title="Warn help",
             description="Warn a user for doing someting bad\nAliases: w\n\n**Required permissions:** Kick Members\n\n**NOTE:** A reason must be provided or the command won't work",
@@ -176,7 +165,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command()
-    async def ban(self, ctx, *arg):
+    async def ban(self, ctx):
         embed = discord.Embed(
             title="Ban help",
             description="Bans a user permanently\nAliases: b\n\n**Required permissions:** Ban Members\n\n**NOTE:** A reason must be provided or the command won't work",
@@ -186,7 +175,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command()
-    async def kick(self, ctx, *arg):
+    async def kick(self, ctx):
         embed = discord.Embed(
             title="Kick help",
             description="Kicks a user out of the server. They are able to come back to the server\nAliases: k\n\n**Required permissions:** Kick Members\n\n**NOTE:** A reason must be provided or the command won't work",
@@ -196,7 +185,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command()
-    async def unban(self, ctx, *arg):
+    async def unban(self, ctx):
         embed = discord.Embed(
             title="Unban help",
             description="Unbans a user so they can be able to come back to the server\nAliases: unb\n\n**Required permissions:** Ban Members\n\n**NOTE:**The user's name and tag must be used to unban them or the command won't work",
@@ -205,7 +194,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['tc', 'textchannel'])
-    async def text_channel(self, ctx, *arg):
+    async def text_channel(self, ctx):
         embed = discord.Embed(title="Text Channel help",
                               description="**Permission required:** Manage Channels", color=0x002aff)
         embed.add_field(name="Create Text Channel",
@@ -220,7 +209,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['vc', 'voicechannel'])
-    async def voice_channel(self, ctx, *arg):
+    async def voice_channel(self, ctx):
         embed = discord.Embed(title="Voice Channel help",
                               description="**Permission required:** Manage Channels", color=0x002aff)
         embed.add_field(name="Create Voice Channel",
@@ -235,7 +224,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['r'])
-    async def role(self, ctx, *arg):
+    async def role(self, ctx):
         embed = discord.Embed(title="Role help",
                               description="**Permission required:** Manage Roles", color=0x002aff)
         embed.add_field(name="Create Role",
@@ -250,7 +239,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command()
-    async def combine(self, ctx, *arg):
+    async def combine(self, ctx):
         embed = discord.Embed(
             title="Combine help",
             description="Type two words to get one combined word",
@@ -260,7 +249,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['meme'])
-    async def animeme(self, ctx, *arg):
+    async def animeme(self, ctx):
         embed = discord.Embed(
             title="Animeme help",
             description="Get some animemes\nAliases: meme, animememe",
@@ -270,7 +259,7 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(aliases=['cat'])
-    async def category(self, ctx, *arg):
+    async def category(self, ctx):
         embed = discord.Embed(title="Category help",
                               description="**Permission required:** Manage Channels", color=0x002aff)
         embed.add_field(name="Create Category",
@@ -285,13 +274,23 @@ class help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(name='bot mutuals', aliases=['bm'])
-    async def botmutuals(self, ctx, *arg):
+    async def botmutuals(self, ctx):
         embed = discord.Embed(
             title="Bot Mutuals help",
             description="See where which servers Nero is in\n\n**NOTE:** This is an ***OWNER ONLY*** command which means only my creator can use it\nAliases: bm",
             color=0x002aff)
         embed.add_field(
             name="Example", value="re!bm", inline=False)
+        await ctx.send(embed=embed)
+
+    @help.command(aliases=['coinflip, headsortails, piece'])
+    async def flip(self, ctx):
+        embed = discord.Embed(
+            title="Flip help",
+            description="Flip a coin and get your result\n\nAliases: coinflip, headsortails, piece",
+            color=0x002aff)
+        embed.add_field(
+            name="Example", value="re!flip heads/tails", inline=False)
         await ctx.send(embed=embed)
 
 
