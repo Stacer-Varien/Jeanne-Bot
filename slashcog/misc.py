dc0033c3dc7cd292be76c1e8426dae6816115b1f
ZaneRE544 <82768608+ZaneRE544@users.noreply.github.com>
2021-10-22 23:16:38 +0200

    Links are now URL buttons for invite command

--- slashcog/misc.py
+++ slashcog/misc.py
@@ -1,7 +1,9 @@
 import discord
+from discord import Embed
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
-
+from discord_slash.utils.manage_components import create_button, create_actionrow
+from discord_slash.model import ButtonStyle
 
 class misc(commands.Cog):
     def __init__(self, bot):
@@ -9,25 +11,41 @@ class misc(commands.Cog):
 
     @cog_ext.cog_slash(description="Invite me to your server or join my creator's servers")
     async def invite(self, ctx:SlashContext):
-        embed = discord.Embed(
+        buttons = [
+            create_button(
+                style=ButtonStyle.URL,
+                label="Bot Invite",
+                url="https://discord.com/api/oauth2/authorize?client_id=831993597166747679&permissions=2550197270&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D831993597166747679%26scope%3Dbot&scope=bot%20applications.commands"
+            ),
+            create_button(
+                style=ButtonStyle.URL,
+                label="Top.gg",
+                url="https://top.gg/bot/831993597166747679"
+            ),
+            create_button(
+                style=ButtonStyle.URL,
+                label="DiscordBots",
+                url="https://discord.bots.gg/bots/831993597166747679"
+            ),
+            create_button(
+                style=ButtonStyle.URL,
+                label="HAZE Server",
+                url="https://discord.gg/VVxGUmqQhF"
+            ),
+            create_button(
+                style=ButtonStyle.URL,
+                label="Jeanne Support Server",
+                url="https://discord.gg/Xn3EvGcMrF"
+            ),]
+
+        action_row=create_actionrow(*buttons)
+
+        embed = Embed(
             title="Invite me!",
-            description="Click on one of these URLs to invite me to you server or join my creator's servers",
+            description="Click on one of these buttons to invite me to you server or join my creator's servers",
             color=0x00bfff)
-        embed.add_field(name="Bot Invite",
-                        value="[Click here](https://discord.com/api/oauth2/authorize?client_id=831993597166747679&permissions=2550197270&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D831993597166747679%26scope%3Dbot&scope=bot%20applications.commands)",
-                        inline=True)
-        embed.add_field(name="Top.gg",
-                        value="[Click here](https://top.gg/bot/831993597166747679)",
-                        inline=True)
-        embed.add_field(name="DiscordBots",
-                    value="[Click here](https://discord.bots.gg/bots/831993597166747679)", inline=True)
-        embed.add_field(name="DiscordBotList",
-                        value="[Click here](https://discordbotlist.com/bots/Jeanne-3694)", inline=True)
-        embed.add_field(name="HAZE server",
-                        value="[Click here](https://discord.gg/VVxGUmqQhF)", inline=True)
-        embed.add_field(name="Jeanne Support Server",
-                        value="[Click here](https://discord.gg/Xn3EvGcMrF)", inline=True)
-        await ctx.send(embed=embed)
+
+        await ctx.send(embed=embed, components=[action_row])
 
     @cog_ext.cog_slash(description="Type a message and I will say it but it will be in plain text")
     @commands.has_permissions(administrator=True)
