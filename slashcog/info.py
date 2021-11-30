9d75e2b6407632cfeca6c7cae3e0126c7b3ac496
ZaneRE544 <garyjordaan912@gmail.com>
2021-11-30 22:12:43 +0200

    Made the stats and moderations mobile friendly

--- slashcog/info.py
+++ slashcog/info.py
@@ -1,6 +1,6 @@
 import discord, time, sys
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
+from discord_slash import cog_ext
 from discord import Member, Embed
 
 
@@ -14,26 +14,26 @@ class info(commands.Cog):
     @cog_ext.cog_slash(description="See the bot's status from development to now")
     async def stats(self, ctx):
         botowner = self.bot.get_user(597829930964877369)
-        stats = Embed(title="Bot stats", color=0x236ce1)
-        stats.add_field(
-            name="General Information", value=f"🡺 **Name:** {self.bot.user}\n🡺 **ID:** {self.bot.user.id}", inline=False)
-        stats.add_field(
-            name="Developer", value=f"🡺 **Name:** {botowner}\n🡺 **ID:** {botowner.id}")
+        embed = Embed(title="Bot stats", color=0x236ce1)
+        embed.add_field(
+            name="General Information", value=f"**>** **Name:** {self.bot.user}\n**>** **ID:** {self.bot.user.id}", inline=False)
+        embed.add_field(
+            name="Developer", value=f"**>** **Name:** {botowner}\n**>** **ID:** {botowner.id}")
         # this is an empty field
-        stats.add_field(name="_ _", value="_ _", inline=False)
-        stats.add_field(
-            name="Version", value=f"🡺 **Python Version:** {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}\n🡺 **Discord.py Version:** {discord.__version__}", inline=True)
-        stats.add_field(name="Count",
-                        value=f"🡺 **Server Count:** {len(ctx.bot.guilds)} servers\n🡺 **User Count:** {len(set(ctx.bot.get_all_members()))}", inline=True)
+        embed.add_field(name="_ _", value="_ _", inline=False)
+        embed.add_field(
+            name="Version", value=f"**>** **Python Version:** {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}\n**>** **Discord.py Version:** {discord.__version__}", inline=True)
+        embed.add_field(name="Count",
+                        value=f"**>** **Server Count:** {len(ctx.bot.guilds)} servers\n**>** **User Count:** {len(set(ctx.bot.get_all_members()))}", inline=True)
         # this is an empty field
-        stats.add_field(name="_ _", value="_ _", inline=False)
-        stats.add_field(name="Ping",
-                        value=f"🡺 **Bot Latency:** {round(ctx.bot.latency * 1000)}ms", inline=True)
-        stats.add_field(
-            name="🡺 **Uptime:**", value="[Click Here](https://status.watchbot.app/bot/831993597166747679)\nPowered by [WatchBot](https://watchbot.app/)")
-        stats.set_thumbnail(
+        embed.add_field(name="_ _", value="_ _", inline=False)
+        embed.add_field(name="Ping",
+                        value=f"**>** **Bot Latency:** {round(ctx.bot.latency * 1000)}ms", inline=True)
+        embed.add_field(
+            name="Uptime", value="[Click Here](https://status.watchbot.app/bot/831993597166747679)\nPowered by [WatchBot](https://watchbot.app/)")
+        embed.set_thumbnail(
             url=self.bot.user.avatar_url)
-        await ctx.send(embed=stats)
+        await ctx.send(embed=embed)
 
     @cog_ext.cog_slash(description="See the information of a member or yourself")
     async def userinfo(self, ctx, member: Member = None):
@@ -53,12 +53,12 @@ class info(commands.Cog):
         userinfo = Embed(title="{}'s Info".format(member.name),
                          color=0xccff33)
         userinfo.add_field(name="General Information",
-                           value=f"🡺 **Name:** {member}\n🡺 **Nickname:** {member.nick}\n🡺 **ID:** {member.id}\n🡺 **Creation Date:** {member.created_at.strftime(format)}\n🡺 **Is Bot?:** {botr}",
+                           value=f"**>** **Name:** {member}\n**>** **Nickname:** {member.nick}\n**>** **ID:** {member.id}\n**>** **Creation Date:** {member.created_at.strftime(format)}\n**>** **Is Bot?:** {botr}",
                            inline=True)
         userinfo.add_field(name="Member Information",
-                           value=f"🡺 **Joined Server:** {member.joined_at.strftime(format)}\n🡺 **Number of Roles:** {len(member.roles)}",
+                           value=f"**>** **Joined Server:** {member.joined_at.strftime(format)}\n**>** **Number of Roles:** {len(member.roles)}",
                            inline=True)
-        userinfo.add_field(name="🡺 **Roles Held**",
+        userinfo.add_field(name="Roles Held",
                            value=''.join(hasroles) + '@everyone', inline=False)
         userinfo.set_thumbnail(url=member.avatar_url)
         await ctx.send(embed=userinfo)
@@ -70,9 +70,9 @@ class info(commands.Cog):
         features = [str(x) for x in guild.features]
 
         if len(emojis) > 10:
-            emojis = emojis[:10]
+            emojilist = emojis[:10]
         elif len(emojis) == 0:
-            emojis = "None"
+            emojilist = "None"
 
         if guild.premium_subscription_count < 2:
             boostlevel = "Level 0"
@@ -86,30 +86,19 @@ class info(commands.Cog):
             boostlevel = "Level 3"
 
         embed = Embed(title="Server's Info", color=0x00B0ff)
-        embed.add_field(name="Server Name", value=ctx.guild.name, inline=True)
-        embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
-        embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
-        embed.add_field(name="Owner ID", value=ctx.guild.owner.id, inline=True)
-        embed.add_field(name="Region", value=ctx.guild.region, inline=True)
-        embed.add_field(name="Members", value=len(
-            ctx.guild.members), inline=True)
-        embed.add_field(name="Creation Date",
-                        value=ctx.guild.created_at.strftime(format),
+        embed.add_field(name="General Information",
+                        value=f"**>** **Name:** {guild.name}\n**>** **ID:** {guild.id}\n**>** **Creation Date:** {guild.created_at.strftime(format)}\n**>** **Member Count:** {len(guild.members)}\n**>** **Verification:** {guild.verification_level}\n**>** **Roles:** {len(guild.roles)}", inline=True)
+        embed.add_field(
+            name="Owner", value=f"**>** **Name:**{guild.owner}\n**>** **ID:** {guild.owner.id}", inline=True)
+        embed.add_field(name="_ _", value="_ _", inline=False)
+        embed.add_field(name="Boost Status",
+                        value=f"**>** **Boosters:** {len(guild.premium_subscribers)}\n**>** **Boosts:** {guild.premium_subscription_count}\n**>** **Boost Level:** {boostlevel}",
                         inline=True)
-        embed.add_field(name="Verification",
-                        value=ctx.guild.verification_level,
-                        inline=True)
-        embed.add_field(name="Boosts",
-                        value=ctx.guild.premium_subscription_count,
-                        inline=True)
-        embed.add_field(name="Boost Level",
-                        value=boostlevel,
-                        inline=True)
-        embed.add_field(name="Roles", value=len(guild.roles), inline=True)
-        embed.add_field(name="Emojis", value=len(emojis), inline=True)
-        embed.add_field(name='Emojis (first 10)',
-                        value="".join(emojis), inline=False)
-        embed.add_field(name="Features", value=features, inline=False)
+        embed.add_field(
+            name="Emojis", value=f"**>** **Number of Emojis:** {len(emojis)}\n{''.join(emojilist)}", inline=True)
+        embed.add_field(name="_ _", value="_ _", inline=False)
+        embed.add_field(name='Features',
+                        value=features, inline=False)
 
         embed.set_thumbnail(url=guild.icon_url)
         embed.set_image(url=guild.splash_url)
@@ -118,7 +107,6 @@ class info(commands.Cog):
 
     @cog_ext.cog_slash(description="Check how fast I respond to a command")
     async def ping(self, ctx):
-
         start_time = time.time()
         test = Embed(description="Testing Ping...", colour=0x236ce1)
         message = await ctx.send(embed=test)
@@ -126,9 +114,9 @@ class info(commands.Cog):
 
         ping = Embed(color=0x236ce1)
         ping.add_field(
-            name="🡺 Bot Latency", value=f'{round(ctx.bot.latency * 1000)}ms', inline=False)
+            name="**>** Bot Latency", value=f'{round(ctx.bot.latency * 1000)}ms', inline=False)
         ping.add_field(
-            name="🡺 API Latency", value=f'{round((end_time - start_time) * 1000)}ms', inline=False)
+            name="**>** API Latency", value=f'{round((end_time - start_time) * 1000)}ms', inline=False)
         await message.edit(content="Ping results", embed=ping)
 
     @cog_ext.cog_slash(description="See the server's banner")
@@ -149,11 +137,12 @@ class info(commands.Cog):
             await ctx.send(embed=embed)
 
     @cog_ext.cog_slash(description="See your avatar or another member's avatar")
-    async def avatar(self, ctx):
-        author = ctx.author
+    async def avatar(self, ctx, member:Member = None):
+        if member==None:
+            member=ctx.author
 
-        avatar = Embed(title=f"{author}'s Avatar", color=0x236ce1)
-        avatar.set_image(url=author.avatar_url)
+        avatar = Embed(title=f"{member}'s Avatar", color=0x236ce1)
+        avatar.set_image(url=member.avatar_url)
         await ctx.send(embed=avatar)
 
 def setup(bot):
