f87a717a10c2bd85dcdd5ad1e827d8550d812167
ZaneRE544 <garyjordaan912@gmail.com>
2021-11-30 22:13:24 +0200

    Improved and added slash commands on owner module

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Embed, Activity, ActivityType
 from discord.ext import commands
 from discord import User, Guild
 
@@ -7,48 +8,42 @@ class owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['fuser'], pass_context=True)
+    @commands.command(aliases=['fuser'])
     @commands.is_owner()
-    async def finduser(self, ctx, user: User = None):
-        embed = discord.Embed(title="User Found", color=0xccff33)
-        embed.add_field(name="Name",
-                        value=user,
-                        inline=True)
-        embed.add_field(name="ID", value=user.id, inline=True)
-        embed.add_field(name="Creation Date",
-                        value=user.created_at.strftime(format),
-                        inline=True)
-        embed.add_field(name="Mutuals", value=len(
-            user.mutual_guilds), inline=True)
-        embed.set_image(url=user.avatar_url)
-        await ctx.send(embed=embed)
+    async def finduser(self, ctx, user:User):
+        if user.bot == True:
+            botr = ":o:"
+        else:
+            botr = ":x:"
 
-    @commands.command(aliases=['fserver'], pass_context=True)
+        fuser = Embed(title="User Found", color=0xccff33)
+        fuser.add_field(name="User Information",
+                           value=f"**>** **Name:** {user}\n**>** **ID:** {user.id}\n**>** **Creation Date:** {user.created_at.strftime(format)}\n**>** **Mutuals:** {len(user.mutual_guilds)}\n**>** **Is Bot?:** {botr}",
+                           inline=True)
+        fuser.set_image(url=user.avatar_url)
+        await ctx.send(embed=fuser)
+
+    
+
+    @commands.command(aliases=['fserver'])
     @commands.is_owner()
-    async def findserver(self, ctx: commands.Context, guild: Guild):
-        embed = discord.Embed(color=0x00B0ff)
-        embed.set_author(name="Server Found")
-        embed.add_field(name="Server Name", value=guild.name, inline=True)
-        embed.add_field(name="Owner", value=guild.owner, inline=True)
-        embed.add_field(name="Owner ID", value=guild.owner.id, inline=True)
-        embed.add_field(name="ID", value=guild.id, inline=True)
-        embed.add_field(name="Region", value=guild.region, inline=True)
-        embed.add_field(name="Members", value=len(guild.members), inline=True)
-        embed.add_field(name="Creation Date",
-                        value=guild.created_at.strftime(format),
-                        inline=True)
-        embed.add_field(name="Verification",
-                        value=guild.verification_level,
-                        inline=True)
-        embed.add_field(name="Server Features",
-                        value=guild.features, inline=True)
-        embed.set_thumbnail(url=guild.icon_url)
-        await ctx.send(embed=embed)
+    async def findserver(self, ctx, *, guild: Guild):
+        fserver = Embed(color=0x00B0ff)
+        fserver.set_author(name="Server Found")
+        fserver.add_field(name="General Information",
+                        value=f"**>** **Name:** {guild.name}\n**>** **ID:** {guild.id}\n**>** **Creation Date:** {guild.created_at.strftime(format)}\n**>** **Member Count:** {len(guild.members)}\n**>** **Verification:** {guild.verification_level}\n**>** **Roles:** {len(guild.roles)}", inline=True)
+        fserver.add_field(
+            name="Owner", value=f"**>** **Name:**{guild.owner}\n**>** **ID:** {guild.owner.id}", inline=True)
+        fserver.add_field(name="_ _", value="_ _", inline=False)
+        fserver.add_field(name="**>** **Server Features**",
+                        value=guild.features, inline=False)
+        fserver.set_thumbnail(url=guild.icon_url)
+        await ctx.send(embed=fserver)
 
     @commands.command(pass_context=True)
     @commands.is_owner()
-    async def mutuals(self, ctx, user: User):
-        embed = discord.Embed(title="Mutual Servers".format(user.name),
+    async def mutuals(self, ctx, *, user: User):
+        embed = Embed(title="Mutual Servers",
                               color=0xF7FF00)
         embed.add_field(name="Name", value=user, inline=True)
         embed.add_field(name="Mutuals", value=len(
@@ -60,27 +55,25 @@ class owner(commands.Cog):
     @commands.is_owner()
     async def botmutuals(self, ctx):
         mutuals = [str(x) for x in ctx.bot.guilds]
-        embed = discord.Embed(color=0xF7FF00)
+        embed = Embed(color=0xF7FF00)
         embed.add_field(name="Mutuals", value=len(ctx.bot.guilds), inline=True)
-        if len(mutuals) > 25:
-            mutuals = mutuals[:25]
-        embed.add_field(name="Servers", value=" \n".join(
+        if len(mutuals) > 1024:
+            mutuals = mutuals[:1024]
+        embed.add_field(name="Servers", value="[ ]".join(
             mutuals), inline=False)
         await ctx.send(embed=embed)
 
 
     @commands.command()
     @commands.is_owner()
-    async def play(self, ctx, *, activity):
-        await ctx.bot.change_presence(activity=discord.Game(name=activity))
-        await ctx.send(f"Bot's activity changed to {activity}")
+    async def activity(self, ctx, activitytype, *, activity):
 
-
-    @commands.command()
-    @commands.is_owner()
-    async def listen(self, ctx, *, activity):
-        await ctx.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity))
-        await ctx.send(f"Bot's activity changed to {activity}")
+        if activitytype=="listen":
+            await ctx.bot.change_presence(activity=Activity(type=ActivityType.listening, name=activity))
+            await ctx.send(f"Bot's activity changed to `listening to {activity}`")
+        elif activitytype=="play":
+            await ctx.bot.change_presence(activity=discord.Game(name=activity))
+            await ctx.send(f"Bot's activity changed to `playing {activity}`")
 
 def setup(bot):
     bot.add_cog(owner(bot))
