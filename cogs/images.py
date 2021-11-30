6dff8d91e880e7336916078741f2399af6048d4d
ZaneRE544 <garyjordaan912@gmail.com>
2021-11-30 22:11:52 +0200

    Updated image module and made wallpapers SFW

--- cogs/images.py
+++ cogs/images.py
@@ -1,8 +1,7 @@
-import discord
-import random
-import requests
-from discord import Embed
+import random, requests, glob
+from discord import Embed, File
 from discord.ext import commands
+
 
 class images(commands.Cog):
     def __init__(self, bot):
@@ -18,48 +17,40 @@ class images(commands.Cog):
         await ctx.send(embed=kitsune)
 
     @commands.command()
-    @commands.is_nsfw()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def wallpaper(self, ctx):
-        wallpaper_api = requests.get(
-            "https://nekos.life/api/v2/img/wallpaper").json()
+        file_path_type = ["./Images/Wallpaper/*.jpg"]
+        images = glob.glob(random.choice(file_path_type))
+        random_image = random.choice(images)
+        file = File(random_image)
         wallpaper = Embed(color=0xFFC0CB)
-        wallpaper.set_footer(text="Fetched from nekos.life")
-        wallpaper.set_image(url=wallpaper_api["url"])
-        await ctx.send(embed=wallpaper)
+        wallpaper.set_footer(text="Powered by JeanneBot")
+        await ctx.send(file=file, embed=wallpaper)
 
-    @commands.command()
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def jeanne(self, ctx):
-        jeanne_api_yandere = random.choice(requests.get(
-            "https://yande.re/post.json?tags=rating:safe+jeanne_d'arc").json())
-
-        jeanne_gelbooru_api = random.choice(requests.get(
-            "https://gelbooru.com//index.php?page=dapi&s=post&q=index&json=1&tags=rating:safe +jeanne_d'arc_(fate)").json())
-
-        jeanne_images=[jeanne_api_yandere, jeanne_gelbooru_api]   
-
-        jeanne = discord.Embed(color=0xFFC0CB)
-        jeanne.set_image(url=random.choice(jeanne_images)["file_url"])
-        jeanne.set_footer(text="Jeanne d'Arc")
-        await ctx.send(embed=jeanne)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def saber(self, ctx):
-        saber_api_yandere = random.choice(requests.get(
-            "https://yande.re/post.json?tags=rating:safe+saber").json())
+        file_path_type = ["./Images/Saber/*.png",
+                          "./Images/Saber/*.jpeg", "./Images/Saber/*.jpg"]
+        images = glob.glob(random.choice(file_path_type))
+        random_image = random.choice(images)
+        file = File(random_image)
+        saber = Embed(color=0xFFC0CB)
+        saber.set_footer(text="Powered by JeanneBot")
+        await ctx.send(file=file, embed=saber)
 
-        saber_gelbooru_api = random.choice(requests.get(
-            "https://gelbooru.com//index.php?page=dapi&s=post&q=index&json=1&tags=rating:safe +saber").json())
-
-        saber_images = [saber_api_yandere, saber_gelbooru_api]
-
-        saber = discord.Embed(color=0xFFC0CB)
-        saber.set_image(url=random.choice(saber_images)["file_url"])
-        saber.set_footer(text="Saber")
-        await ctx.send(embed=saber)
-
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def jeanne(self, ctx):
+        file_path_type = ["./Images/Jeanne/*.png",
+                          "./Images/Jeanne/*.jpeg", "./Images/Jeanne/*.jpg"]
+        images = glob.glob(random.choice(file_path_type))
+        random_image = random.choice(images)
+        file=File(random_image)
+        jeanne = Embed(color=0xFFC0CB)
+        jeanne.set_footer(text="Powered by JeanneBot")
+        await ctx.send(file=file, embed=jeanne)
 
 
 def setup(bot):
