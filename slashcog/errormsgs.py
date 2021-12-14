2994fe4cec068cd5e17e2e7d4cfc6117f3a981a9
ZaneRE544 <82768608+ZaneRE544@users.noreply.github.com>
2021-12-14 11:32:41 +0200

    Added slash error on outsideban and finduser

--- slashcog/errormsgs.py
+++ slashcog/errormsgs.py
@@ -1,6 +1,6 @@
-import discord
+from discord import Embed
 from discord.ext import commands
-from discord.ext.commands.errors import NotOwner, NSFWChannelRequired, NotOwner
+from discord.ext.commands.errors import NotOwner, NSFWChannelRequired, NotOwner, UserNotFound
 
 
 class errors(commands.Cog):
@@ -10,15 +10,19 @@ class errors(commands.Cog):
     @commands.Cog.listener()
     async def on_slash_command_error(self, ctx, error):
         if isinstance(error, NotOwner):
-            embed = discord.Embed(
+            embed = Embed(
             title="Owner only command", description="This command failed to commit because you are not the bot owner", color=0xff0000)
             await ctx.send(embed=embed)
         elif isinstance(error, NSFWChannelRequired):
-            error = discord.Embed(
+            error = Embed(
                 title='NSFW Failed', description="NSFW material couldn't be sent in this channel", color=0xff0000)
             error.add_field(
                 name="Reason", value="Channel is not NSFW enabled")
             await ctx.send(embed=error)
+        elif isinstance(error, UserNotFound):
+            no_user = Embed(
+                title="User does not exist", description="Please make sure the USER_ID is valid or maybe they have deleted their account.", color=0xff0000)
+            await ctx.send(embed=no_user)            
 
 
 def setup(bot):
