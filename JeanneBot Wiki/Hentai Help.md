6f5b0005d265d69a1813dbeb03c0ad266de935b9
ZaneRE544 <garyjordaan912@gmail.com>
2021-12-27 15:10:25 +0200

    Replaced NSFW with hentai and added new commands

---
+++ JeanneBot Wiki/Hentai Help.md
@@ -0,0 +1,43 @@
+# Hentai
+
+**NOTE:** All commands in this module requires an **NSFW enabled channel**. Certain tags have been blacklisted for the sake of the viewer and/or due to Discord's ToS. All tags must be put in the format the APIs requires.
+
+>**Hentai**
+
+* Get a random hentai image from Yande.re, Gelbooru, Danbooru or Jeanne. Fow now, you can't include a tag until the command has been improved.
+* **NOTE:** The prefix command fetch the images from the APIs and the slash command fetch the hentai from Jeanne
+
+    Example: `j!hentai`
+
+>**Hentai+**
+
+* Get 3 random hentai images from Yande.re, Gelbooru or Danbooru. Fow now, you can't include a tag until the command has been improved.
+* **NOTE:** This is a prefix only command. The slash command for it will be out soon
+
+    Example: `j!hentai+`
+
+>**Yandere**
+
+* Get a random hentai image from Yande.re. You can include a tag too for a specific hentai
+
+    Example: `j!yandere` (for a random hentai) \ `j!yandere TAG` (for a specific tag)
+
+>**Gelbooru**
+
+* Get a random hentai image from Gelbooru. You can include a tag too for a specific hentai
+
+    Example: `j!gelbooru` (for a random hentai) \ `j!gelbooru TAG` (for a specific tag)
+
+>**Danbooru**
+
+* Get a random hentai image from Danbooru. You can include a tag too for a specific hentai
+
+    Example: `j!danbooru` (for a random hentai) \ `j!danbooru TAG` (for a specific tag)
+
+>**Report Loli**
+
+* In case if I somehow didn't know the hentai material had a loli, it will be reported and logged into the console and be removed
+* **NOTE:** This command has a 1 hour cooldown to prevent trolls from reporting false positives. You should include media links of the content
+* **Aliases:** report_loli
+
+    Example: `j!report_loli MEDIA_LINK`
--- slashcog/images.py
+++ slashcog/images.py
@@ -1,58 +1,58 @@
-import random
-import requests
-import glob
 from discord import Embed, File
-from discord.ext import commands
-from discord_slash import cog_ext
+from discord.ext.commands import Cog
+from discord_slash.cog_ext import cog_slash as jeanne_slash
+from glob import glob
+from random import choice
+from requests import get
 
 
-class images(commands.Cog):
+class images(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(description="Get a kitsune image")
+    @jeanne_slash(description="Get a kitsune image")
     async def kitsune(self, ctx):
-        kistune_api = requests.get("https://nekos.life/api/v2/img/fox_girl").json()
+        kistune_api = get("https://nekos.life/api/v2/img/fox_girl").json()
         kitsune = Embed(color=0xFFC0CB)
         kitsune.set_footer(text="Fetched from nekos.life")
         kitsune.set_image(url=kistune_api["url"])
         await ctx.send(embed=kitsune)
 
-    @cog_ext.cog_slash(description="Need a wallpaper for your PC?")
+    @jeanne_slash(description="Need a wallpaper for your PC?")
     async def wallpaper(self, ctx):
-        file_path_type = ["./Images/Wallpaper/*.jpg"]
-        images = glob.glob(random.choice(file_path_type))
-        random_image = random.choice(images)
+        file_path_type = ["./Media/Wallpaper/*.jpg"]
+        images = glob(choice(file_path_type))
+        random_image = choice(images)
         file = File(random_image)
         wallpaper = Embed(color=0xFFC0CB)
         wallpaper.set_footer(text="Powered by JeanneBot")
         await ctx.send(file=file, embed=wallpaper)
 
-    @cog_ext.cog_slash(description="Get a random Jeanne d'Arc image")
+    @jeanne_slash(description="Get a random Jeanne d'Arc image")
     async def jeanne(self, ctx):
-        file_path_type = ["./Images/Jeanne/*.jpg"]
-        images = glob.glob(random.choice(file_path_type))
-        random_image = random.choice(images)
+        file_path_type = ["./Media/Jeanne/*.jpg"]
+        images = glob(choice(file_path_type))
+        random_image = choice(images)
         file = File(random_image)
         jeanne = Embed(color=0xFFC0CB)
         jeanne.set_footer(text="Powered by JeanneBot")
         await ctx.send(file=file, embed=jeanne)
 
-    @cog_ext.cog_slash(description="Get a random Saber image")
+    @jeanne_slash(description="Get a random Saber image")
     async def saber(self, ctx):
-        file_path_type = ["./Images/Saber/*.jpg"]
-        images = glob.glob(random.choice(file_path_type))
-        random_image = random.choice(images)
+        file_path_type = ["./Media/Saber/*.jpg"]
+        images = glob(choice(file_path_type))
+        random_image = choice(images)
         file = File(random_image)
         saber = Embed(color=0xFFC0CB)
         saber.set_footer(text="Powered by JeanneBot")
         await ctx.send(file=file, embed=saber)
 
-    @cog_ext.cog_slash(description="Get a random neko image")
+    @jeanne_slash(description="Get a random neko image")
     async def neko(self, ctx):
-        file_path_type = ["./Images/Neko/*.jpg"]
-        images = glob.glob(random.choice(file_path_type))
-        random_image = random.choice(images)
+        file_path_type = ["./Media/Neko/*.jpg"]
+        images = glob(choice(file_path_type))
+        random_image = choice(images)
         file = File(random_image)
         neko = Embed(color=0xFFC0CB)
         neko.set_footer(text="Powered by JeanneBot")
