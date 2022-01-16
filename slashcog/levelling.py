9cd84fa7fcba528f3431956ace105f79a6418f5c
ZaneRE544 <garyjordaan912@gmail.com>
2022-01-16 22:02:59 +0200

    2hr fix on levelling system. XP is now global

--- slashcog/levelling.py
+++ slashcog/levelling.py
@@ -1,32 +1,10 @@
+import imp
 from nextcord import SlashOption, Member, slash_command as jeanne_slash, Interaction, Embed
 from nextcord.ext.commands import Cog
 from json import load, dump
+from asyncio import sleep
 
 
-async def update_data(users, user, guild):
-    if not str(guild.id) in users:
-        users[str(guild.id)] = {}
-        if not str(user.id) in users[str(guild.id)]:
-            users[str(guild.id)][str(user.id)] = {}
-            users[str(guild.id)][str(user.id)]['experience'] = 0
-            users[str(guild.id)][str(user.id)]['level'] = 1
-    elif not str(user.id) in users[str(guild.id)]:
-        users[str(guild.id)][str(user.id)] = {}
-        users[str(guild.id)][str(user.id)]['experience'] = 0
-        users[str(guild.id)][str(user.id)]['level'] = 1
-
-
-async def add_experience(users, user, exp):
-  users[str(user.guild.id)][str(user.id)]['experience'] += exp
-
-
-async def level_up(users, user):
-  experience = users[str(user.guild.id)][str(user.id)]['experience']
-  lvl_start = users[str(user.guild.id)][str(user.id)]['level']
-  lvl_end = int(experience ** (1/4))
-
-  if lvl_start < lvl_end:
-      users[str(user.guild.id)][str(user.id)]['level'] = lvl_end
 
 class levelling(Cog):
     def __init__(self, bot):
@@ -38,7 +16,7 @@ class levelling(Cog):
         with open('users.json', 'r') as f:
             users = load(f)
 
-        await update_data(users, member, member.guild)
+        await self.update_data(users, member)
 
         with open('users.json', 'w') as f:
             dump(users, f)
@@ -46,35 +24,64 @@ class levelling(Cog):
 
     @Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot:
-            with open('level.json', 'r') as f:
-                    users = load(f)
-            await update_data(users, message.author, message.guild)
-            await add_experience(users, message.author, 5)
-            await level_up(users, message.author)
+        if message.author.bot == False:
+            with open('users.json', 'r') as f:
+                users = load(f)
 
-            with open('level.json', 'w') as f:
+            await self.update_data(users, message.author)
+            await self.add_experience(users, message.author, 5)
+            await self.level_up(users, message.author)
+
+            with open('users.json', 'w') as f:
                 dump(users, f)
 
+        await self.bot.process_commands(message)
+
+
+    async def update_data(self, users, user):
+        if not f'{user.id}' in users:
+            users[f'{user.id}'] = {}
+            users[f'{user.id}']['experience'] = 0
+            users[f'{user.id}']['level'] = 1
+
+
+    async def add_experience(self, users, user, exp):
+        users[f'{user.id}']['experience'] += exp
+        await sleep(60)
+
+
+    async def level_up(self, users, user):
+        with open('levels.json', 'r') as g:
+            levels = load(g)
+        experience = users[f'{user.id}']['experience']
+        lvl_start = users[f'{user.id}']['level']
+        lvl_end = int(experience ** (1 / 4))
+        if lvl_start < lvl_end:
+            users[f'{user.id}']['level'] = lvl_end
+
     @jeanne_slash(description="See your level or someone else's level")
-    async def level(self, interaction:Interaction, member: Member = SlashOption(required=False)):
-        guild=interaction.guild
-        if member==None:
+    async def level(self, interaction: Interaction, member: Member = SlashOption(required=False)):
+
+        if not member:
             member = interaction.user
-                    
-        with open('level.json','r') as f:
-            users = load(f)
-        lvl = users[str(guild.id)][str(interaction.user.id)]['level']
-        exp = users[str(guild.id)][str(interaction.user.id)]['experience']
+        
+        member_id=member.id
+        with open('users.json','r') as f:
+                users = load(f)
+        lvl = users[str(member_id)]['level']
+        exp = users[str(member_id)]['experience']
+
         boxes = int((exp/(100*((1/2) * lvl)))*10)
 
-        embed=Embed(title=f"{member}'s Level Stats", color=0x00FF00)
-        embed.add_field(name="XP", value=f"{exp}/{int(100*((1/2)*lvl))}", inline=True)
+        embed = Embed(title=f"{member}'s Level Stats", color=0x00FF00)
+        embed.add_field(
+            name="XP", value=f"{exp}/{int(100*((1/2)*lvl))}", inline=True)
         embed.add_field(name="Level", value=f"Level {lvl}")
         embed.add_field(name="Progress Bar", value=boxes * ":blue_square:" + (
             10-boxes) * ":white_large_square:", inline=False)
         embed.set_thumbnail(url=member.display_avatar)
         await interaction.response.send_message(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(levelling(bot))            
