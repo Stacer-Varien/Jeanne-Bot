7f58d1529e1af26ed80ee39d3ad9d54a9d4bf9d3
ZaneRE544 <82768608+ZaneRE544@users.noreply.github.com>
2022-01-22 12:33:03 +0200

    Switched from JSON to SQLITE for levelling system

--- slashcog/levelling.py
+++ slashcog/levelling.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import imp
 from nextcord import SlashOption, Member, slash_command as jeanne_slash, Interaction, Embed
 from nextcord.ext.commands import Cog
@@ -87,3 +88,77 @@ class levelling(Cog):
 def setup(bot):
     bot.add_cog(levelling(bot))            
 
+=======
+from math import sqrt
+from aiosqlite import connect
+from nextcord.ext.commands import Cog
+from nextcord import slash_command as jeanne_slash, Embed, Interaction, Member, SlashOption, Colour
+
+
+class levelling(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+
+    async def initialize(self):
+        await self.bot.wait_until_ready()
+        db = await connect("expData.db")
+        await db.execute("CREATE TABLE IF NOT EXISTS guildData (guild_id int, user_id int, exp int, PRIMARY KEY (guild_id, user_id))")
+
+    @Cog.listener()
+    async def on_message(self, message):
+        db = await connect("expData.db")
+        multiplier=5
+        if not message.author.bot:
+            cursor = await db.execute("INSERT OR IGNORE INTO guildData (guild_id, user_id, exp) VALUES (?,?,?)", (message.guild.id, message.author.id, 5))
+
+            if cursor.rowcount == 0:
+                await db.execute("UPDATE guildData SET exp = exp + 5 WHERE guild_id = ? AND user_id = ?", (message.guild.id, message.author.id))
+                cur = await db.execute("SELECT exp FROM guildData WHERE guild_id = ? AND user_id = ?", (message.guild.id, message.author.id))
+                data = await cur.fetchone()
+                exp = data[0]
+                lvl = sqrt(exp) / multiplier
+
+                if lvl.is_integer():
+                    pass
+
+            await db.commit()
+
+        await self.bot.process_commands(message)
+
+    @jeanne_slash(description="See your level or someone else's level")
+    async def level(self, interaction: Interaction, member: Member = SlashOption(required=None)):
+        db = await connect("expData.db")
+        multiplier=5
+        if member is None:
+            member = interaction.user
+
+        # get user exp
+        async with db.execute("SELECT exp FROM guildData WHERE guild_id = ? AND user_id = ?", (interaction.guild.id, member.id)) as cursor:
+            data = await cursor.fetchone()
+            exp = data[0]
+
+            # calculate rank
+        async with db.execute("SELECT exp FROM guildData WHERE guild_id = ?", (interaction.guild.id,)) as cursor:
+            rank = 1
+            async for value in cursor:
+                if exp < value[0]:
+                    rank += 1
+
+        lvl = int(sqrt(exp)//multiplier)
+        next_lvl_exp = (multiplier*((lvl+1)))**2
+        lvl_percent= (exp / next_lvl_exp)*100
+
+        embed = Embed(
+            title=f"Stats for {member.name}")
+        embed.add_field(name="Level", value=str(lvl))
+        embed.add_field(name="Exp", value=f"{exp}/{next_lvl_exp}")
+        embed.add_field(name="Rank", value=f"{rank}/{interaction.guild.member_count}")
+        embed.add_field(name="Level Progress", value=f"{round(lvl_percent, 2)}%")
+        await interaction.response.send_message(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(levelling(bot))            
+
+>>>>>>> Stashed changes
