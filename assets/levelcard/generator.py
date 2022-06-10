4a6e18891157931c337e3848cf1957ba44060b69
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2022-06-10 23:12:37 +0200

    Made a level card
    I've forked disrank and modified it to capture data from the database

---
+++ assets/levelcard/generator.py
@@ -0,0 +1,123 @@
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+import requests
+import math
+import os
+
+class Generator:
+    def __init__(self):
+        self.default_bg = os.path.join(os.path.dirname(__file__), 'assets', 'card.png')
+        self.font1      = os.path.join(os.path.dirname(__file__), 'assets', 'font.ttf')
+        self.font2      = os.path.join(os.path.dirname(__file__), 'assets', 'font2.ttf')
+
+    def generate_profile(self, bg_image: str = None, profile_image: str = None, server_level: int = 0, server_current_xp: int = 5, server_user_xp: int = 5, server_next_xp: int = 100, global_level: int = 0, global_current_xp: int = 5, global_user_xp: int = 5, global_next_xp: int = 100, user_name: str = 'Varien#1956'):
+        if not bg_image:
+            card = Image.open(self.default_bg).convert("RGBA")
+        else:
+            bg_bytes = BytesIO(requests.get(bg_image).content)
+            card = Image.open(bg_bytes).convert("RGBA")
+
+            width, height = card.size
+            if width == 900 and height == 500:
+                pass
+            else:
+                x1 = 0
+                y1 = 0
+                x2 = width
+                nh = math.ceil(width * 0.264444)
+                y2 = 0
+
+                if nh < height:
+                    y1 = (height / 2) - 119
+                    y2 = nh + y1
+
+                card = card.crop((x1, y1, x2, y2)).resize((900, 500))
+
+        profile_bytes = BytesIO(requests.get(profile_image).content)
+        profile = Image.open(profile_bytes)
+        profile = profile.convert('RGBA').resize((180, 180))
+
+        profile_pic_holder = Image.new(
+            "RGBA", card.size, (255, 255, 255, 0)
+        )
+
+        mask = Image.new("RGBA", card.size, 0)
+        mask_draw = ImageDraw.Draw(mask)
+        mask_draw.ellipse(
+            (29, 29, 209, 209), fill=(255, 25, 255, 255)
+        )
+
+        # ======== Fonts to use =============
+        font_normal = ImageFont.truetype(self.font1, 40)
+        font_small = ImageFont.truetype(self.font1, 30)
+
+        # ======== Colors ========================
+        DARK = (204, 204, 255)
+
+        def get_str(xp):
+            if xp < 1000:
+                return str(xp)
+            if xp >= 1000 and xp < 1000000:
+                return str(round(xp / 1000, 1)) + "k"
+            if xp > 1000000:
+                return str(round(xp / 1000000, 1)) + "M"
+
+        draw = ImageDraw.Draw(card)
+        draw.text((245, 22), user_name, DARK, font=font_normal)
+        draw.text((245, 98), f"Server Level: {server_level}", DARK, font=font_small)
+        draw.text(
+            (500, 98),
+            f"Server XP: {get_str(server_user_xp)}/{get_str(server_next_xp)}",
+            DARK,
+            font=font_small,
+        )
+
+        blank = Image.new("RGBA", card.size, (255, 255, 255, 0))
+        blank_draw = ImageDraw.Draw(blank)
+        blank_draw.rectangle(
+            (245, 155, 710, 165), fill=(255, 255, 255, 0), outline=DARK
+        )
+
+        server_xpneed = server_next_xp - server_current_xp
+        server_xphave = server_user_xp - server_current_xp
+
+        server_current_percentage = (server_xphave / server_xpneed) * 100
+        server_length_of_bar = (server_current_percentage * 4.9) + 208
+
+        blank_draw.rectangle((248, 158, server_length_of_bar, 162), fill=DARK)        
+        blank_draw.ellipse((20, 20, 218, 218), fill=(255, 255, 255, 0), outline=DARK)
+
+########################################################################################
+
+        draw.text((245, 208), f"Global Level: {global_level}", DARK, font=font_small)
+        draw.text(
+            (500, 208),
+            f"Global XP: {get_str(global_user_xp)}/{get_str(global_next_xp)}",
+            DARK,
+            font=font_small,
+        )
+
+        blank_draw.rectangle(
+            (245, 265, 710, 275), fill=(255, 255, 255, 0), outline=DARK
+        )
+
+        global_xpneed = global_next_xp - global_current_xp
+        global_xphave = global_user_xp - global_current_xp
+
+        global_current_percentage = (global_xphave / global_xpneed) * 100
+        global_length_of_bar = (global_current_percentage * 4.9) + 318
+
+        blank_draw.rectangle((248, 268, global_length_of_bar, 272), fill=DARK)
+
+        profile_pic_holder.paste(profile, (29, 29, 209, 209))
+
+        pre = Image.composite(profile_pic_holder, card, mask)
+        pre = Image.alpha_composite(pre, blank)
+
+        blank = Image.new("RGBA", pre.size, (255, 255, 255, 0))
+
+        final = Image.alpha_composite(pre, blank)
+        final_bytes = BytesIO()
+        final.save(final_bytes, 'png')
+        final_bytes.seek(0)
+        return final_bytes
--- cogs/levelling.py
+++ cogs/levelling.py
@@ -1,12 +1,24 @@
-from nextcord.ext.commands import Cog
+from asyncio import get_event_loop
+from functools import partial
+from nextcord.ext.commands import Cog, CooldownMapping, BucketType
 from nextcord import *
 from nextcord import slash_command as jeanne_slash
-from random import randrange
 from config import db
+from typing import Optional
+from assets.levelcard.generator import Generator
 
 class levelling(Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._cd = CooldownMapping.from_cooldown(1, 120, BucketType.member)
+
+    def get_ratelimit(self, message: Message) -> Optional[int]:
+        bucket = self._cd.get_bucket(message)
+        return bucket.update_rate_limit()
+
+    def get_card(self, args):
+        image = Generator().generate_profile(**args)
+        return image
 
     @Cog.listener()
     async def on_message(self, message):
@@ -19,60 +31,60 @@ class levelling(Cog):
             if message.author.id == botbanned_user.id:
                 pass
         except:
-            if not message.author.bot:
+         ratelimit = self.get_ratelimit(message)
+         if ratelimit is None:
+            if not message.author.bot: 
                 cursor1 = db.execute("INSERT OR IGNORE INTO serverxpData (guild_id, user_id, lvl, exp, cumulative_exp) VALUES (?,?,?,?,?)", (
                     message.guild.id, message.author.id, 0, 5, 5))
 
                 cursor2 = db.execute(
                     "INSERT OR IGNORE INTO globalxpData (user_id, lvl, exp, cumulative_exp) VALUES (?,?,?,?)", (message.author.id, 0, 5, 5))
-
-                xp = randrange(5, 10)
+                xp = 5
                 if cursor1.rowcount == 0:
-                    scurrent_exp_query = db.execute(
+                        scurrent_exp_query = db.execute(
                         f"SELECT exp, cumulative_exp, lvl FROM serverxpData WHERE user_id = {message.author.id}")
-                    scurrent_exp_data = scurrent_exp_query.fetchone()
-                    scurrent_exp = scurrent_exp_data[0]
-                    scurrent_cumulative_exp = scurrent_exp_data[1]
-                    scurrent_lvl = scurrent_exp_data[2]
+                        scurrent_exp_data = scurrent_exp_query.fetchone()
+                        scurrent_exp = scurrent_exp_data[0]
+                        scurrent_cumulative_exp = scurrent_exp_data[1]
+                        scurrent_lvl = scurrent_exp_data[2]
 
-                    supdated_exp = scurrent_exp + xp
-                    supdated_cumulative_exp = scurrent_cumulative_exp + xp
-                    snext_lvl_exp = ((scurrent_lvl * 100) +
-                                     ((scurrent_lvl - 1) * 50) + 100)
+                        supdated_exp = scurrent_exp + xp
+                        supdated_cumulative_exp = scurrent_cumulative_exp + xp
+                        snext_lvl_exp = ((scurrent_lvl * 50) +
+                                         ((scurrent_lvl - 1) * 25) + 50)
 
-                    if supdated_cumulative_exp >= snext_lvl_exp:
-                        supdated_exp = supdated_cumulative_exp - snext_lvl_exp
-                        db.execute(
+                        if supdated_cumulative_exp >= snext_lvl_exp:
+                            supdated_exp = supdated_cumulative_exp - snext_lvl_exp
+                            db.execute(
                             f"UPDATE serverxpData SET lvl = lvl + 1, exp = {supdated_exp} WHERE guild_id = {message.guild.id} AND user_id = {message.author.id}")
 
-                    db.execute(
+                        db.execute(
                         f"UPDATE serverxpData SET exp = {supdated_exp}, cumulative_exp = {supdated_cumulative_exp} WHERE guild_id = {message.guild.id} AND user_id = {message.author.id}")
 
                 db.commit()
 
                 if cursor2.rowcount == 0:
-                    gcurrent_exp_query = db.execute(
-                        f"SELECT exp, cumulative_exp, lvl FROM globalxpData WHERE user_id = {message.author.id}")
-                    gcurrent_exp_data = gcurrent_exp_query.fetchone()
-                    gcurrent_exp = gcurrent_exp_data[0]
-                    gcurrent_cumulative_exp = gcurrent_exp_data[1]
-                    gcurrent_lvl = gcurrent_exp_data[2]
+                        gcurrent_exp_query = db.execute(
+                            f"SELECT exp, cumulative_exp, lvl FROM globalxpData WHERE user_id = {message.author.id}")
+                        gcurrent_exp_data = gcurrent_exp_query.fetchone()
+                        gcurrent_exp = gcurrent_exp_data[0]
+                        gcurrent_cumulative_exp = gcurrent_exp_data[1]
+                        gcurrent_lvl = gcurrent_exp_data[2]
 
-                    gupdated_exp = gcurrent_exp + xp
-                    gupdated_cumulative_exp = gcurrent_cumulative_exp + xp
-                    gnext_lvl_exp = ((gcurrent_lvl * 100) +
-                                     ((gcurrent_lvl - 1) * 50) + 100)
+                        gupdated_exp = gcurrent_exp + xp
+                        gupdated_cumulative_exp = gcurrent_cumulative_exp + xp
+                        gnext_lvl_exp = ((gcurrent_lvl * 50) +
+                                         ((gcurrent_lvl - 1) * 25) + 50)
 
-                    if gupdated_cumulative_exp >= gnext_lvl_exp:
-                        gupdated_exp = gupdated_cumulative_exp - gnext_lvl_exp
+                        if gupdated_cumulative_exp >= gnext_lvl_exp:
+                            gupdated_exp = gupdated_cumulative_exp - gnext_lvl_exp
+                            db.execute(
+                                f"UPDATE globalxpData SET lvl = lvl + 1, exp = {gupdated_exp} WHERE user_id = {message.author.id}")
+
                         db.execute(
-                            f"UPDATE globalxpData SET lvl = lvl + 1, exp = {gupdated_exp} WHERE user_id = {message.author.id}")
-
-                    db.execute(
-                        f"UPDATE globalxpDATA SET exp = {gupdated_exp}, cumulative_exp = {gupdated_cumulative_exp} WHERE user_id = {message.author.id}")
+                            f"UPDATE globalxpDATA SET exp = {gupdated_exp}, cumulative_exp = {gupdated_cumulative_exp} WHERE user_id = {message.author.id}")
 
                 db.commit()
-
             await self.bot.process_commands(message)
 
     @jeanne_slash(description="See your level or someone else's level")
@@ -88,45 +100,42 @@ class levelling(Cog):
                 pass
         except:
             if member is None:
-                member = ctx.user
+                    member = ctx.user
             try:
-                query1 = db.execute(
+                squery = db.execute(
                     f"SELECT * FROM serverxpData where guild_id = {ctx.guild.id} AND user_id = {member.id}")
-                query1_data = query1.fetchone()
-                slvl = query1_data[2]
-                sexp = query1_data[3]
+                squery_data = squery.fetchone()
+                slvl = squery_data[2]
+                sexp = squery_data[3]
 
-                s_next_lvl_exp = ((slvl * 100) + ((slvl - 1) * 50) + 100)
+                gquery = db.execute(
+                    f"SELECT * FROM globalxpData where user_id = {member.id}")
+                gquery_data = gquery.fetchone()
+                glvl = gquery_data[1]
+                gexp = gquery_data[2]
 
-                blue_box = int(round((sexp / s_next_lvl_exp) * 10))
-                swhite_box = 10 - blue_box
 
-                query2 = db.execute(
-                    f"SELECT lvl FROM globalxpData where user_id = {member.id}")
-                query2_data = query2.fetchone()
-                glvl = query2_data[0]
+                args = {
+                    'bg_image': '',
+ 			            'profile_image': str(member.avatar.with_format('png')),
+ 			            'server_level': slvl,
+ 			            'server_user_xp': sexp,
+ 			            'server_next_xp': ((slvl * 50) + ((slvl - 1) * 25) + 50),
+                        'global_level': glvl,
+ 			            'global_user_xp': gexp,
+ 			            'global_next_xp': ((glvl * 50) + ((glvl - 1) * 25) + 50),
+ 			            'user_name': str(member),
+                        }
 
-                gexp_query = db.execute(
-                    f"SELECT exp FROM globalxpData WHERE user_id = {member.id}")
-                gexp_data = gexp_query.fetchone()
-                gexp = gexp_data[0]
+                func = partial(self.get_card, args)
+                image = await get_event_loop().run_in_executor(None, func)
 
-                g_next_lvl_exp = ((glvl * 100) + ((glvl - 1) * 50) + 100)
-
-                red_box = int(round((gexp / g_next_lvl_exp) * 10))
-                gwhite_box = 10 - red_box
-
-                embed = Embed(title=f"{member}'s level stats", color=0x008000)
-                embed.add_field(name="**__Server__**",
-                                value=f"\n**>** **Level:** {slvl}\n**>** **Experience:** {sexp}\{s_next_lvl_exp}XP\n**>** **Experience to Next Level:** {s_next_lvl_exp - sexp}XP", inline=True)
-                embed.add_field(name="**__Global__**",
-                                value=f"\n**>** **Level:** {glvl}\n**>** **Experience:** {gexp}\{g_next_lvl_exp}XP\n**>** **Experience to Next Level:** {g_next_lvl_exp - gexp}XP", inline=True)
-                embed.add_field(
-                    name="**__Progress__**", value=f"**>** **Server:** {blue_box * ':blue_square:' + swhite_box * ':white_large_square:'}\n**>** **Global:** {red_box * ':red_square:' + gwhite_box * ':white_large_square:'}", inline=False)
-                await ctx.followup.send(embed=embed)
+                file = File(fp=image, filename=f'{member.name}_level_card.png')
+                await ctx.followup.send(file=file)
             except:
                 no_exp = Embed(description="Failed to get level stats")
                 await ctx.followup.send(embed=no_exp)
+
 
     @jeanne_slash(description="Check the users with the most XP in the server")
     async def rank(self, ctx: Interaction, type=SlashOption(description="Server or Global specific?", choices=["server", "global"])):
