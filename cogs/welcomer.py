165f040ff584cc3dec0f2eddfb506c75b76d3be9
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2022-06-23 22:29:51 +0200

    Remodified welcomer and leaver

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -9,41 +9,21 @@ class welcomer(Cog):
     @Cog.listener()
     async def on_member_join(self, member):
         try:
-            channel_id_query = db.execute(
-                f"SELECT channel_id FROM welcomerData where guild_id = {member.guild.id}")
-            channel_id = channel_id_query.fetchone()[0]
-
             guild_id_query = db.execute(
                 f"SELECT guild_id FROM welcomerData where channel_id = {channel_id}")
             server_id = guild_id_query.fetchone()[0]
 
             server = self.bot.get_guild(server_id)
 
-            try:
-                    channel = self.bot.get_channel(channel_id)
-                    try:
-                        welcome = Embed(color=member.color)
-
-                        if member.avatar !=None:
-                            if member.guild.icon.is_animated() is True:
-                                welcome.set_author(name=f"Hello {member} and welcome to {server.name}!", icon_url=member.guild.icon.with_size(512)).set_thumbnail(url=member.avatar)
-
-                            elif member.guild.icon.is_animated() is False:
-                                welcome.set_author(name=f"Hello {member} and welcome to {server.name}!", icon_url=member.guild.icon).set_thumbnail(url=member.avatar)
-
-                        elif member.avatar == None:
-                            if member.guild.icon.is_animated() is True:
-                                welcome.set_author(name=f"Hello {member} and welcome to {server.name}!", icon_url=member.guild.icon.with_size(512))
-
-                            elif member.guild.icon.is_animated() is False:
-                                welcome.set_author(name=f"Hello {member} and welcome to {server.name}!", icon_url=member.guild.icon)
+            if member.guild.id == server_id:
+                channel_id_query = db.execute(
+                    "SELECT channel_id FROM welcomerData where guild_id = ?", (member.guild.id))
+                channel_id = channel_id_query.fetchone()[0]
+                channel = self.bot.get_channel(channel_id)
+            
+                welcome = Embed(description=f"Hi {member} and welcome to {server.name}!",color=member.color).set_thumbnail(url=member.display_avatar)
                                 
-                        await channel.send(embed=welcome)
-
-                    except:
-                        pass
-            except:
-                    pass
+                await channel.send(embed=welcome)
             else:
                 pass
         except:
@@ -61,35 +41,11 @@ class welcomer(Cog):
             server_id = guild_id_query.fetchone()[0]
 
             if member.guild.id == server_id:
-                try:
-                    channel = self.bot.get_channel(channel_id)
-                    try:
-                        channel = self.bot.get_channel(channel_id)
-                        leave = Embed(color=0x00FFFF)
+                channel = self.bot.get_channel(channel_id)
 
-                        if member.avatar != None:
-                            if member.guild.icon.is_animated() is True:
-                                leave.set_author(name=f"{member} left the server", icon_url=member.guild.icon.with_size(
-                                    512)).set_thumbnail(url=member.avatar)
+                leave = Embed(description=f"{member} left the server", color=0x00FFFF).set_thumbnail(url=member.display_avatar)
 
-                            elif member.guild.icon.is_animated() is False:
-                                leave.set_author(name=f"{member} left the server", icon_url=member.guild.icon).set_thumbnail(
-                                    url=member.avatar)
-
-                        elif member.avatar == None:
-                            if member.guild.icon.is_animated() is True:
-                                leave.set_author(
-                                    name=f"{member} left the server", icon_url=member.guild.icon.with_size(512))
-
-                            elif member.guild.icon.is_animated() is False:
-                                leave.set_author(
-                                    name=f"{member} left the server", icon_url=member.guild.icon)
-
-                        await channel.send(embed=leave)
-                    except:
-                        pass
-                except:
-                    pass
+                await channel.send(embed=leave)
             else:
                 pass
         except:
