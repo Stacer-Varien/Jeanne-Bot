94a3c7276eef34da5315cec47e217541be8e78a5
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2022-07-08 16:27:18 +0200

    Potential fix on vote rewards (test mode)

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -1,4 +1,4 @@
-from config import TOPGG, db, TOPGG_AUTH
+from config import TOPGG, db
 from topgg import *
 from nextcord.ext import tasks
 from nextcord.ext.commands import Cog
@@ -12,7 +12,7 @@ class topgg(Cog):
         self.bot = bot
         self.update_stats.start()
         self.topggpy = DBLClient(self.bot, dbl_token)
-        self.topgg_webhook = WebhookManager(bot).dbl_webhook("/dblwebhook", TOPGG_AUTH)
+        self.topgg_webhook = WebhookManager(self.bot).dbl_webhook("/dblwebhook", "password")
         self.topgg_webhook.run(5000)
 
     @tasks.loop(minutes=30)
@@ -26,31 +26,14 @@ class topgg(Cog):
 
     @Cog.listener()
     async def on_dbl_vote(self, data):        
-        if data["type"] == "upvote":
-            voter = await self.bot.fetch_user(data['user'])
-            try:
-                botbanquery = db.execute(
-                    "SELECT * FROM botbannedData WHERE user_id = ?", (voter.id,))
-                botbanned_data = botbanquery.fetchone()
-                botbanned = botbanned_data[0]
+        if data["type"] == "test":
+            return self.bot.dispatch("dbl_test", data)
 
-                if voter.id == botbanned:
-                    pass
-            except:
-                if await self.topggpy.get_weekend_status() is True:
-                    credits=100
-                else:
-                    credits=50
+        print(f"Received a vote:\n{data}")
 
-            
-                cur = db.execute(
-                "INSERT OR IGNORE INTO bankData (user_id, amount) VALUES (?,?)", (voter.id, int(credits)))
-
-                if cur.rowcount == 0:
-                    db.execute(
-                    f"UPDATE bankData SET amount = amount + 100 WHERE user_id = ?", (voter.id))
-                    db.commit()
-                print(f"Received a vote:\n{data}")
+    @Cog.listener()
+    async def on_dbl_test(data):
+        print(f"Received a test vote:\n{data}")
 
 
 def setup(bot):
