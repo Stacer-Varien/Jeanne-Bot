da49508a16842befd7d84ad68b5c1c02664faaf4
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2022-08-18 14:15:58 +0200

    Fixed the video support on gelbooru cmd

--- cogs/hentai.py
+++ cogs/hentai.py
@@ -1,15 +1,12 @@
 from json import loads
+from random import choice
 from nextcord import *
 from nextcord import slash_command as jeanne_slash
-from glob import glob
-from nextcord.ext.commands import Cog
-from random import choice
-from requests import get
 from nextcord.ext.application_checks import *
+from nextcord.ext.commands import Cog
+from requests import get
 from assets.db_functions import check_botbanned_user
-from config import db
-
-
+from assets.errormsgs import nsfw_perm
 
 
 class slashnsfw(Cog):
@@ -18,72 +15,81 @@ class slashnsfw(Cog):
 
     @jeanne_slash(description="Get a random hentai from Jeanne")
     @is_nsfw()
-    async def hentai(self, ctx: Interaction):
+    async def nsfw(self, ctx: Interaction):
         await ctx.response.defer()
-        check = check_botbanned_user(ctx.user.id)
-        if check == ctx.user.id:
+        if check_botbanned_user(ctx.user.id) == True:
             pass
         else:
             gelbooru_api = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub"
             response = get(gelbooru_api)
             ret = loads(response.text)
-            gelbooru_image = choice(ret['post'])["file_url"] 
+            gelbooru_image = choice(ret['post'])["file_url"]
 
-            yandere_image = choice(get("https://yande.re/post.json?limit=100&tags=rating:explicit+-loli+-shota+-cub").json())['file_url']
+            yandere_image = \
+            choice(get("https://yande.re/post.json?limit=100&tags=rating:explicit+-loli+-shota+-cub").json())[
+                'file_url']
 
-            konachan_image = choice(get("https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub").json())['file_url']
+            konachan_image = choice(
+                get("https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub").json())[
+                'file_url']
 
-            h=[gelbooru_image, yandere_image, konachan_image]
+            h = [gelbooru_image, yandere_image, konachan_image]
 
-            hentai=choice(h)
+            hentai = choice(h)
 
             if hentai == gelbooru_image:
-                source='Gelbooru'
+                source = 'Gelbooru'
 
             elif hentai == yandere_image:
-                source='Yande.re'
+                source = 'Yande.re'
 
             elif hentai == konachan_image:
-                source='Konachan'
+                source = 'Konachan'
 
-            embed = Embed(color=0xFFC0CB).set_image(url=hentai).set_footer(text="Fetched from {}".format(source))
-            await ctx.followup.send(embed=embed)
+            if hentai.endswith('mp4'):
+                await ctx.followup.send(hentai)
+            else:
+                embed = Embed(color=0xFFC0CB).set_image(url=hentai).set_footer(text="Fetched from {}".format(source))
+                await ctx.followup.send(embed=embed)
 
     @jeanne_slash(description="Get a random hentai from Gelbooru")
     @is_nsfw()
-    async def gelbooru(self, ctx: Interaction, tag=SlashOption(description="Add a tag for something specific", required=False)):
+    async def gelbooru(self, ctx: Interaction,
+                       tag=SlashOption(description="Add a tag for something specific", required=False)):
         await ctx.response.defer()
-        check = check_botbanned_user(ctx.user.id)
-        if check == ctx.user.id:
+        if check_botbanned_user(ctx.user.id) == True:
             pass
         else:
             try:
                 if tag == None:
                     gelbooru_api = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub"
                 else:
-                    formated_tag=tag.replace(" ","_")
-                    print(formated_tag)
+                    formated_tag = tag.replace(" ", "_")
                     gelbooru_api = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub+" + formated_tag
-            
+
                 response = get(gelbooru_api)
                 ret = loads(response.text)
                 image = choice(ret['post'])["file_url"]
 
-                embed = Embed(color=0xFFC0CB).set_image(
-                    url=image).set_footer(text="Fetched from Gelbooru")
-                await ctx.followup.send(embed=embed)
+                if image.endswith('mp4'):
+                    await ctx.followup.send(image)
+
+                else:
+
+                    embed = Embed(color=0xFFC0CB).set_image(
+                        url=image).set_footer(text="Fetched from Gelbooru")
+                    await ctx.followup.send(embed=embed)
             except:
                 no_tag = Embed(
-                description="The tag could not be found", color=Color.red())
+                    description="The tag could not be found", color=Color.red())
                 await ctx.followup.send(embed=no_tag)
-
 
     @jeanne_slash(description="Get a random hentai from Yande.re")
     @is_nsfw()
-    async def yandere(self, ctx: Interaction, tag=SlashOption(description="Add a tag for something specific", required=False)):
+    async def yandere(self, ctx: Interaction,
+                      tag=SlashOption(description="Add a tag for something specific", required=False)):
         await ctx.response.defer()
-        check = check_botbanned_user(ctx.user.id)
-        if check == ctx.user.id:
+        if check_botbanned_user(ctx.user.id) == True:
             pass
         else:
             try:
@@ -110,10 +116,10 @@ class slashnsfw(Cog):
 
     @jeanne_slash(description="Get a random hentai from Konachan")
     @is_nsfw()
-    async def konachan(self, ctx: Interaction, tag=SlashOption(description="Add a tag for something specific", required=False)):
+    async def konachan(self, ctx: Interaction,
+                       tag=SlashOption(description="Add a tag for something specific", required=False)):
         await ctx.response.defer()
-        check = check_botbanned_user(ctx.user.id)
-        if check == ctx.user.id:
+        if check_botbanned_user(ctx.user.id) == True:
             pass
         else:
             try:
@@ -138,11 +144,13 @@ class slashnsfw(Cog):
     @Cog.listener()
     async def on_application_command_error(self, ctx: Interaction, error):
         if isinstance(error, ApplicationNSFWChannelRequired):
+            await ctx.response.defer()
             no_hentai = Embed(
                 title='Hentai Failed', description="Hentai couldn't be sent in this channel", color=0xff0000)
             no_hentai.add_field(
-                name="Reason", value=ApplicationNSFWChannelRequired(ctx.channel))
-            await ctx.send(embed=no_hentai)
+                name="Reason", value=nsfw_perm)
+            await ctx.followup.send(embed=no_hentai)
+
 
 def setup(bot):
     bot.add_cog(slashnsfw(bot))
