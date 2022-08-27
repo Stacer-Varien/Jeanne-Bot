fb3f14a96b70adba17d5f9e7cc248ebf8bf78634
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2022-08-27 19:04:55 +0200

    Fixed the inventory bug

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -125,64 +125,83 @@ class inventory(Cog):
         if check_botbanned_user(ctx.user.id) == True:
             pass
         else:
-            balance=db.execute("SELECT amount FROM bankData WHERE user_id = ?", (ctx.user.id,)).fetchone()
+            balance=get_balance(ctx.user.id)
             
 
             if balance == None:
                 nomoney = Embed(description='You have no QP.\nPlease get QP by doing `/daily`, `/guess` and/or `/dice`')
                 await ctx.followup.send(embed=nomoney)
             
-            elif int(balance[0]) < 1000:
+            elif int(balance) < 1000:
                 notenough = Embed(
                     description='You do not have enough QP.\nPlease get more QP by doing `/daily`, `/guess` and/or `/dice`')
                 await ctx.followup.send(embed=notenough)
 
             else:
-                qp = self.bot.get_emoji(980772736861343774)
+                alert="""
+Before you buy a custom background picture, did you make sure the image is:
 
+    1. A valid and permanent image URL
+    2. Not crossing NSFW borderline
+    3. Does not contain any lewded characters and/or lolis
+    4. Is not sexualised in some way
+    5. Does not violate ToS and/or contains TW slurs
+    6. The name you set for it also does not violate ToS and/or contains TW slurs
+    7. The image's resolution is at a 9:5 ratio and at least 900x500 for better quality.
 
-                if requests.get(link).status_code == 200:  
-                    args = {
-                        'bg_image': link,
-             	    	'profile_image': str(ctx.user.avatar.with_format('png')),
-             			'server_level': 100,
-             			'server_user_xp': 50,
-             			'server_next_xp': 100,
-                        'global_level': 100,
-             			'global_user_xp': 100,
-             			'global_next_xp': 100,
-             			'user_name': str(ctx.user),
-                    }
+If you feel that the image fits the above or not, click one of the buttons to continue
+"""
+                view=Confirmation()
+                confirm = Embed(description=alert, color=ctx.user.color)
+                await ctx.followup.send(embed=confirm, view=view)
+                await view.wait()
+                if view.value == True:
+                    loading = self.bot.get_emoji(1012677456811016342)
+                    qp = self.bot.get_emoji(980772736861343774)
+                    await ctx.edit_original_message(content="Creating preview... This will take some time {}".format(loading), view=None, embed=None)
+                    
+                    
+                    if requests.get(link).status_code == 200:  
+                        args = {
+                            'bg_image': link,
+             	        	'profile_image': str(ctx.user.avatar.with_format('png')),
+             	    		'server_level': 100,
+             	    		'server_user_xp': 50,
+             	    		'server_next_xp': 100,
+                            'global_level': 100,
+             	    		'global_user_xp': 100,
+             	    		'global_next_xp': 100,
+             	    		'user_name': str(ctx.user),
+                        }
 
-                    func = partial(self.get_card, args)
-                    image = await get_event_loop().run_in_executor(None, func)
+                        func = partial(self.get_card, args)
+                        image = await get_event_loop().run_in_executor(None, func)
 
-                    file = File(fp=image, filename=f'preview_level_card.png')
+                        file = File(fp=image, filename='preview_level_card.png')
 
-                    preview = Embed(description="This is the preview of the level card.", color=Color.blue()).add_field(name="Cost", value=f"1000 {qp}").set_footer(
-                        text="Is this the background you wanted?").set_footer(text="Please note that if the custom background violates ToS or is NSFW, it will be removed with NO REFUNDS!")
-                    view = Confirmation()
-                    await ctx.followup.send(file=file, embed=preview, view=view)
-                    await view.wait()
+                        preview = Embed(description="This is the preview of the level card.", color=Color.blue()).add_field(name="Cost", value=f"1000 {qp}").set_footer(
+                            text="Is this the background you wanted?").set_footer(text="Please note that if the custom background violates ToS or is NSFW, it will be removed with NO REFUNDS!")
+                        view=Confirmation()
+                        await ctx.edit_original_message(content=None, file=file, embed=preview, view=view)
+                        await view.wait()
 
-                    if view.value is None:
-                        pass
-                    elif view.value is True:
-                        add_user_custom_wallpaper(ctx.user.id, name, link)
+                        if view.value is None:
+                            await ctx.edit_original_message(content="Time out", view=None)
+                        elif view.value is True:
+                            add_user_custom_wallpaper(ctx.user.id, name, link)
 
-                        embed1 = Embed(
-                            description=f"Background wallpaper bought. Don't forget to use `/use {name}` to set it", color=Color.blue())
-                        await ctx.followup.send(embed=embed1)
+                            embed1 = Embed(
+                                description=f"Background wallpaper bought. Don't forget to use `/use {name}` to set it", color=Color.blue())
+                            await ctx.edit_original_message(embed=embed1, view=None)
 
-                    elif view.value is False:
-                        await ctx.edit_original_message(content="Cancelled", embed=None, file=None, view=None)
-
-    @Cog.listener()
-    async def on_application_command_error(self, ctx:Interaction, error):
-        if isinstance(error, ApplicationInvokeError):
-            embed=Embed(description=error, color=Color.red())
-            await ctx.send(embed=embed)
-            
+                        elif view.value is False:
+                            await ctx.edit_original_message(content="Cancelled", embed=None, view=None)
+                elif view.value == False:
+                    check = Embed(description="Please try to sort out the image first and try again", color=Color.blue())
+                    await ctx.edit_original_message(view=None, embed=check)
+                elif view.value == None:
+                    await ctx.edit_original_message(content="Time out", embed=None, view=None)
+           
 
     @jeanne_slash(description='Check which backgrounds you have')
     async def inventory(self, ctx:Interaction):
@@ -195,10 +214,10 @@ class inventory(Cog):
                 await ctx.followup.send("You dont have anything available")
             else:
                 inv=Embed(color=Color.blue()).set_footer(text='To view them, click on the hyperlink')
-                r=1
+                
                 for b in a:
                     inv.add_field(name="Item ID: {}".format(b[0]), value="[View]({})".format(b[1]), inline=True)
-                    r+=1
+                    
 
                 await ctx.followup.send(embed=inv)
                
