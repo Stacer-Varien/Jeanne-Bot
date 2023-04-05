9cdd62a5ee6515253280210772206d19850f638b
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2023-04-05 16:55:59 +0200

    Adding safebooru cmd

--- cogs/image.py
+++ cogs/image.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import lxml.etree as LET
+import requests
 from functions import Botban
 from discord import Color, Embed, Interaction, app_commands as Jeanne
 from discord.ext.commands import Cog, Bot
@@ -13,81 +16,90 @@ class images(Cog):
 
     @Jeanne.command(description="Get a kitsune image")
     async def kitsune(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            kistune_api = get(kitsune_nekoslife).json()
-            kitsune = Embed(color=Color.random())
-            kitsune.set_footer(
-                text="Fetched from nekos.life • Credits must go to the artist")
-            kitsune.set_image(url=kistune_api["url"])
-            await ctx.followup.send(embed=kitsune)
+        kistune_api = get(kitsune_nekoslife).json()
+        kitsune = Embed(color=Color.random())
+        kitsune.set_footer(
+            text="Fetched from nekos.life • Credits must go to the artist")
+        kitsune.set_image(url=kistune_api["url"])
+        await ctx.followup.send(embed=kitsune)
 
     @Jeanne.command(description="Need a wallpaper for your PC or phone?")
     async def wallpaper(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            wallpaper = Embed(color=Color.random())
-            wallpaper.set_image(url=get_wallpaper_pic())
-            wallpaper.set_footer(
-                text=
-                "Fetched from Wallpaper_1936 • Credits must go to the artist")
-            await ctx.followup.send(embed=wallpaper)
+        wallpaper = Embed(color=Color.random())
+        wallpaper.set_image(url=get_wallpaper_pic())
+        wallpaper.set_footer(
+            text=
+            "Fetched from Wallpaper_1936 • Credits must go to the artist")
+        await ctx.followup.send(embed=wallpaper)
 
     @Jeanne.command(description="Get a Jeanne d'Arc image")
     async def jeanne(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            jeanne = Embed(color=Color.random())
-            jeanne.set_image(url=get_jeanne_pic())
-            jeanne.set_footer(
-                text="Fetched from Jeanne_1936 • Credits must go to the artist"
-            )
-            await ctx.followup.send(embed=jeanne)
+        jeanne = Embed(color=Color.random())
+        jeanne.set_image(url=get_jeanne_pic())
+        jeanne.set_footer(
+            text="Fetched from Jeanne_1936 • Credits must go to the artist"
+        )
+        await ctx.followup.send(embed=jeanne)
 
     @Jeanne.command(description="Get a Saber image")
     async def saber(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            saber = Embed(color=Color.random())
-            saber.set_image(url=get_saber_pic())
-            saber.set_footer(
-                text="Fetched from Saber_1936 • Credits must go to the artist")
-            await ctx.followup.send(embed=saber)
+        saber = Embed(color=Color.random())
+        saber.set_image(url=get_saber_pic())
+        saber.set_footer(
+            text="Fetched from Saber_1936 • Credits must go to the artist")
+        await ctx.followup.send(embed=saber)
 
     @Jeanne.command(description="Get a neko image")
     async def neko(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            neko_api = get(neko_purrbot).json()
-            neko = Embed(color=Color.random())
-            neko.set_image(url=neko_api['link'])
-            neko.set_footer(
-                text="Fetched from PurrBot.site • Credits must go to the artist"
-            )
-            await ctx.followup.send(embed=neko)
+        neko_api = get(neko_purrbot).json()
+        neko = Embed(color=Color.random())
+        neko.set_image(url=neko_api['link'])
+        neko.set_footer(
+            text="Fetched from PurrBot.site • Credits must go to the artist"
+        )
+        await ctx.followup.send(embed=neko)
 
     @Jeanne.command(description="Get a Medusa (Fate) image")
     async def medusa(self, ctx: Interaction):
-            await ctx.response.defer()
-            if Botban(ctx.user).check_botbanned_user() == True:
-                return
+        await ctx.response.defer()
+        if Botban(ctx.user).check_botbanned_user() == True:
+            return
 
-            medusa = Embed(color=Color.random())
-            medusa.set_image(url=get_medusa_pic())
-            medusa.set_footer(
-                text="Fetched from Medusa_1936 • Credits must go to the artist"
-            )
-            await ctx.followup.send(embed=medusa)
+        medusa = Embed(color=Color.random())
+        medusa.set_image(url=get_medusa_pic())
+        medusa.set_footer(
+            text="Fetched from Medusa_1936 • Credits must go to the artist"
+        )
+        await ctx.followup.send(embed=medusa)
+
+    @Jeanne.command(description="Get an image from Safebooru")
+    async def safebooru(self, ctx:Interaction, tag:Optional[str]=None):
+        response=requests.get('https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=100&tags=rating:general').text
+        parser = LET.XMLParser(recover=True)
+        tree = LET.ElementTree(LET.fromstring(response, parser=parser))
+        root = tree.getroot()
+        embed=Embed()
+
 
 
 async def setup(bot: Bot):
