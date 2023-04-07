cfdf47613865b15722c88dd9fe826e4c9d021b47
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2023-04-08 00:37:56 +0200

    Added report buttons and menus

--- cogs/hentai.py
+++ cogs/hentai.py
@@ -5,134 +5,131 @@ from discord.ext.commands import Cog, Bot
 from requests import get
 from functions import Botban, Hentai
 from typing import Literal, Optional
-from assets.buttons import ReportContent
-from assets.menus import ReportSelect
+from assets.components import ReportContent, ReportSelect
 
 
 class nsfw(Cog):
-
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @Jeanne.command(description="Get a random hentai from Jeanne", nsfw=True)
     @Jeanne.describe(rating="Do you want questionable or explicit content?")
     async def hentai(
-            self,
-            ctx: Interaction,
-            rating: Optional[Literal["questionable",
-                                     "explicit"]] = None) -> None:
-        await ctx.channel.typing()
+        self,
+        ctx: Interaction,
+        rating: Optional[Literal["questionable", "explicit"]] = None,
+    ) -> None:
+        await ctx.response.defer(thinking=False)
         if Botban(ctx.user).check_botbanned_user() == True:
             return
 
-        if rating == None:
-            rating = ["questionable", "explicit"]
-            rating = choice(rating)
+        hentai, source = Hentai().hentai(rating)
 
-        gelbooru_api = f"https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:{rating}+-loli+-shota+-cub"
-        response = get(gelbooru_api)
-        ret = loads(response.text)
-        gelbooru_image = choice(ret['post'])["file_url"]
-
-        yandere_image = choice(
-            get(f"https://yande.re/post.json?limit=100&tags=rating:{rating}+-loli+-shota+-cub"
-                ).json())['file_url']
-
-        konachan_image = choice(
-            get(f"https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:{rating}+-loli+-shota+-cub"
-                ).json())['file_url']
-
-        h = [gelbooru_image, yandere_image, konachan_image]
-
-        hentai: str = choice(h)
-
-        if hentai == gelbooru_image:
-            source = 'Gelbooru'
-
-        elif hentai == yandere_image:
-            source = 'Yande.re'
-
-        elif hentai == konachan_image:
-            source = 'Konachan'
-
-        if hentai.endswith('mp4'):
-            await ctx.channel.send(hentai)
+        if hentai.endswith("mp4"):
+            view = ReportContent(hentai)
+            await ctx.followup.send(hentai)
 
         else:
-            embed = Embed(color=Color.purple()).set_image(
-                url=hentai).set_footer(
-                    text="Fetched from {} • Credits must go to the artist".
-                    format(source))
-            await ctx.channel.send(embed=embed)
+            embed = (
+                Embed(color=Color.purple())
+                .set_image(url=hentai)
+                .set_footer(
+                    text="Fetched from {} • Credits must go to the artist".format(
+                        source
+                    )
+                )
+            )
+            view = ReportContent(hentai)
+            await ctx.followup.send(embed=embed, view=view)
 
-
-    @Jeanne.command(description="Get a random media content from Gelbooru",
-                    nsfw=True)
-    @Jeanne.describe(rating="Do you want questionable or explicit content?",
-                     tag="Add your tag", plus="Need more content? (up to 4)")
-    async def gelbooru(self,
-                       ctx: Interaction,
-                       rating: Optional[Literal["questionable", "explicit"]],
-                       tag: Optional[str] = None, plus:Optional[bool]=None) -> None:
-        await ctx.channel.typing()
+    @Jeanne.command(description="Get a random media content from Gelbooru", nsfw=True)
+    @Jeanne.describe(
+        rating="Do you want questionable or explicit content?",
+        tag="Add your tag",
+        plus="Need more content? (up to 4)",
+    )
+    async def gelbooru(
+        self,
+        ctx: Interaction,
+        rating: Optional[Literal["questionable", "explicit"]],
+        tag: Optional[str] = None,
+        plus: Optional[bool] = None,
+    ) -> None:
+        await ctx.response.defer(thinking=False)
         if Botban(ctx.user).check_botbanned_user() == True:
             return
 
         image = Hentai(plus).gelbooru(rating, tag)
 
-        if plus==True:
-            view = ReportSelect(image[0]['file_url'], image[1]['file_url'], image[2]['file_url'], image[3]['file_url'])
-            if 'mp4' in str(image):
-                media=[image['file_url'] for image in image]
-                await ctx.channel.send("\n".join(media), view=view)
+        if plus == True:
+            view = ReportSelect(
+                image[0]["file_url"],
+                image[1]["file_url"],
+                image[2]["file_url"],
+                image[3]["file_url"],
+            )
+            if "mp4" in str(image)[:4]:
+                media = [image["file_url"] for image in image][:4]
+                await ctx.followup.send("\n".join(media), view=view)
             else:
                 color = Color.random()
-                embed1 = Embed(
-                    color=color, url='https://gelbooru.com'
-                ).set_image(url=image[0]['file_url']).set_footer(
-                    text="Fetched from Gelbooru • Credits must go to the artist")
-                embed2 = Embed(
-                    color=color,
-                    url='https://gelbooru.com').set_image(url=image[1]['file_url'])
-                embed3 = Embed(
-                    color=color,
-                    url='https://gelbooru.com').set_image(url=image[2]['file_url'])
-                embed4 = Embed(
-                    color=color,
-                    url='https://gelbooru.com').set_image(url=image[3]['file_url'])
+                embed1 = (
+                    Embed(color=color, url="https://gelbooru.com")
+                    .set_image(url=image[0]["file_url"])
+                    .set_footer(
+                        text="Fetched from Gelbooru • Credits must go to the artist"
+                    )
+                )
+                embed2 = Embed(color=color, url="https://gelbooru.com").set_image(
+                    url=image[1]["file_url"]
+                )
+                embed3 = Embed(color=color, url="https://gelbooru.com").set_image(
+                    url=image[2]["file_url"]
+                )
+                embed4 = Embed(color=color, url="https://gelbooru.com").set_image(
+                    url=image[3]["file_url"]
+                )
                 embeds = [embed1, embed2, embed3, embed4]
-                await ctx.channel.send(embeds=embeds, view=view)
+                await ctx.followup.send(embeds=embeds, view=view)
         else:
-            view=ReportContent(image)
-            if image.endswith('mp4'):
-                await ctx.channel.send(image, view=view)
+            view = ReportContent(image)
+            if image.endswith("mp4"):
+                await ctx.followup.send(image, view=view)
 
             else:
-                embed = Embed(color=Color.purple(
-                )).set_image(url=image).set_footer(
-                    text="Fetched from Gelbooru • Credits must go to the artist"
+                embed = (
+                    Embed(color=Color.purple())
+                    .set_image(url=image)
+                    .set_footer(
+                        text="Fetched from Gelbooru • Credits must go to the artist"
+                    )
                 )
                 view = ReportContent(image)
-                await ctx.channel.send(embed=embed, view=view)
+                await ctx.followup.send(embed=embed, view=view)
 
     @gelbooru.error
-    async def gelbooru_error(self, ctx: Interaction,
-                             error: Jeanne.AppCommandError):
+    async def gelbooru_error(self, ctx: Interaction, error: Jeanne.AppCommandError):
         if isinstance(error, Jeanne.CommandInvokeError):
             if IndexError or KeyError:
-                no_tag = Embed(description="The tag could not be found",
-                               color=Color.red())
-                await ctx.followup.send(embed=no_tag)
+                no_tag = Embed(
+                    description="The tag could not be found", color=Color.red()
+                )
+                await ctx.channel.send(embed=no_tag)
 
     @Jeanne.command(description="Get a random hentai from Yande.re", nsfw=True)
-    @Jeanne.describe(rating="Do you want questionable or explicit content?",
-                     tag="Add your tag",
-                     plus="Need more content? (up to 4)")
-    async def yandere(self,
-                      ctx: Interaction,
-                      rating: Optional[Literal["questionable", "explicit"]],
-                      tag: Optional[str] = None, plus:Optional[bool]=None) -> None:
-        await ctx.response.defer()
+    @Jeanne.describe(
+        rating="Do you want questionable or explicit content?",
+        tag="Add your tag",
+        plus="Need more content? (up to 4)",
+    )
+    async def yandere(
+        self,
+        ctx: Interaction,
+        rating: Optional[Literal["questionable", "explicit"]],
+        tag: Optional[str] = None,
+        plus: Optional[bool] = None,
+    ) -> None:
+        await ctx.response.defer(thinking=False)
         if Botban(ctx.user).check_botbanned_user() == True:
             return
 
@@ -145,85 +142,110 @@ class nsfw(Cog):
             image = Hentai(plus).yandere(rating, tag)
 
             if plus == True:
-                color = Color.random()
-                embed1 = Embed(
-                    color=color, url='https://yande.re'
-                ).set_image(url=image[0]['file_url']).set_footer(
-                    text="Fetched from Yande.re • Credits must go to the artist"
+                view = ReportSelect(
+                image[0]["file_url"],
+                image[1]["file_url"],
+                image[2]["file_url"],
+                image[3]["file_url"],
                 )
-                embed2 = Embed(color=color,
-                               url='https://yande.re').set_image(
-                                   url=image[1]['file_url'])
-                embed3 = Embed(color=color,
-                               url='https://yande.re').set_image(
-                                   url=image[2]['file_url'])
-                embed4 = Embed(color=color,
-                               url='https://yande.re').set_image(
-                                   url=image[3]['file_url'])
+                color = Color.random()
+                embed1 = (
+                    Embed(color=color, url="https://yande.re")
+                    .set_image(url=image[0]["file_url"])
+                    .set_footer(
+                        text="Fetched from Yande.re • Credits must go to the artist"
+                    )
+                )
+                embed2 = Embed(color=color, url="https://yande.re").set_image(
+                    url=image[1]["file_url"]
+                )
+                embed3 = Embed(color=color, url="https://yande.re").set_image(
+                    url=image[2]["file_url"]
+                )
+                embed4 = Embed(color=color, url="https://yande.re").set_image(
+                    url=image[3]["file_url"]
+                )
                 embeds = [embed1, embed2, embed3, embed4]
-                await ctx.followup.send(embeds=embeds)
+                await ctx.followup.send(embeds=embeds, view=view)
             else:
                 yandere = Embed(color=Color.random())
-                yandere.set_image(url=image['file_url'])
+                yandere.set_image(url=image["file_url"])
                 yandere.set_footer(
-                    text="Fetched from Yande.re • Credits must go to the artist")
-                await ctx.followup.send(embed=yandere)
+                    text="Fetched from Yande.re • Credits must go to the artist"
+                )
+                view = ReportContent(image)
+                await ctx.followup.send(embed=yandere, view=view)
 
     @yandere.error
-    async def yandere_error(self, ctx: Interaction,
-                            error: Jeanne.AppCommandError):
+    async def yandere_error(self, ctx: Interaction, error: Jeanne.AppCommandError):
         if isinstance(error, Jeanne.CommandInvokeError):
             if IndexError or KeyError:
-                no_tag = Embed(description="The tag could not be found",
-                               color=Color.red())
+                no_tag = Embed(
+                    description="The tag could not be found", color=Color.red()
+                )
                 await ctx.followup.send(embed=no_tag)
 
     @Jeanne.command(description="Get a random hentai from Konachan")
-    @Jeanne.describe(rating="Do you want questionable or explicit content?",
-                     tag="Add your tag", plus="Need more content (up to 4)")
-    async def konachan(self,
-                       ctx: Interaction,
-                       rating: Optional[Literal["questionable", "explicit"]],
-                       tag: Optional[str] = None, plus:Optional[bool]=None) -> None:
-        await ctx.response.defer()
+    @Jeanne.describe(
+        rating="Do you want questionable or explicit content?",
+        tag="Add your tag",
+        plus="Need more content (up to 4)",
+    )
+    async def konachan(
+        self,
+        ctx: Interaction,
+        rating: Optional[Literal["questionable", "explicit"]],
+        tag: Optional[str] = None,
+        plus: Optional[bool] = None,
+    ) -> None:
+        await ctx.response.defer(thinking=False)
         if Botban(ctx.user).check_botbanned_user() == True:
             return
 
         image = Hentai(plus).konachan(rating, tag)
 
         if plus == True:
+            view = ReportSelect(
+                image[0]["file_url"],
+                image[1]["file_url"],
+                image[2]["file_url"],
+                image[3]["file_url"],
+                )            
             color = Color.random()
-            embed1 = Embed(
-                color=color, url='https://konachan.com'
-            ).set_image(url=image[0]['file_url']).set_footer(
-                text="Fetched from Yande.re • Credits must go to the artist"
+            embed1 = (
+                Embed(color=color, url="https://konachan.com")
+                .set_image(url=image[0]["file_url"])
+                .set_footer(
+                    text="Fetched from Yande.re • Credits must go to the artist"
+                )
             )
-            embed2 = Embed(
-                color=color,
-                url='https://konachan.com').set_image(url=image[1]['file_url'])
-            embed3 = Embed(
-                color=color,
-                url='https://konachan.com').set_image(url=image[2]['file_url'])
-            embed4 = Embed(
-                color=color,
-                url='https://konachan.com').set_image(url=image[3]['file_url'])
+            embed2 = Embed(color=color, url="https://konachan.com").set_image(
+                url=image[1]["file_url"]
+            )
+            embed3 = Embed(color=color, url="https://konachan.com").set_image(
+                url=image[2]["file_url"]
+            )
+            embed4 = Embed(color=color, url="https://konachan.com").set_image(
+                url=image[3]["file_url"]
+            )
             embeds = [embed1, embed2, embed3, embed4]
-            await ctx.followup.send(embeds=embeds)
+            await ctx.followup.send(embeds=embeds, view=view)
         else:
             konachan = Embed(color=Color.random())
-            konachan.set_image(url=image['file_url'])
+            konachan.set_image(url=image["file_url"])
             konachan.set_footer(
                 text="Fetched from Konachan • Credits must go to the artist"
             )
-            await ctx.followup.send(embed=konachan)
+            view = ReportContent(image)
+            await ctx.followup.send(embed=konachan, view=view)
 
     @konachan.error
-    async def konachan_error(self, ctx: Interaction,
-                             error: Jeanne.AppCommandError):
+    async def konachan_error(self, ctx: Interaction, error: Jeanne.AppCommandError):
         if isinstance(error, Jeanne.CommandInvokeError):
             if IndexError or KeyError:
-                no_tag = Embed(description="The tag could not be found",
-                               color=Color.red())
+                no_tag = Embed(
+                    description="The tag could not be found", color=Color.red()
+                )
                 await ctx.followup.send(embed=no_tag)
 
 
