28b70fa93eeba686f3e4deb3ad2a32ab8eff41f5
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-27 12:16:49 +0200

    Redid the TopGG cog

--- cogs/topgg_.py
+++ cogs/topgg_.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Cog, Bot
 from datetime import datetime
 
 
-class topgg(Cog):
+class TopGG(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self.topggpy = DBLClient(bot=self.bot, token=TOPGG, autopost=True)
@@ -21,16 +21,16 @@ class topgg(Cog):
     async def update_stats(self):
         try:
             print(
-                f"Posted server count ({self.topggpy.guild_count}) at {datetime.now().strftime('%H:%M')}"
+                f"Posted server count ({self.topggpy.guild_count()}) at {datetime.now().strftime('%H:%M')}"
             )
         except Exception as e:
             print(f"Failed to post server count\n{e.__class__.__name__}: {e}")
 
     @Cog.listener()
-    async def on_dbl_vote(self, data: dict):
+    async def on_dbl_vote(self, data):
         if data["type"] == "upvote":
             voter = await self.bot.fetch_user(data["user"])
-            if Botban(voter).check_botbanned_user() == True:
+            if Botban(voter).check_botbanned_user():
                 return
 
             if await self.topggpy.get_weekend_status() == True:
@@ -42,11 +42,9 @@ class topgg(Cog):
             print(f"Received a vote:\n{data}")
             with open("voter_data.json", "r") as f:
                 json_dict = "".join(f.readlines())
-            dict = loads(json_dict)
-
-            data.update(dict)
+            data.update(loads(json_dict))
             dumps(data)
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(topgg(bot))
+    await bot.add_cog(TopGG(bot))
