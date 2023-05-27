efa99818afd2beddd73a6d909578b68bb27c9bf4
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-27 12:09:59 +0200

    Redid the message logger cog

--- cogs/logger.py
+++ cogs/logger.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Cog, Bot
 from functions import Logger
 
 
-class logger(Cog):
+class LoggerCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
@@ -15,26 +15,22 @@ class logger(Cog):
         if before.author.bot:
             return
 
-        if not (logger_id := Logger(after.guild).get_message_logger()):
+        logger_id = Logger(after.guild).get_message_logger()
+
+        if not logger_id:
             return
 
-        channel = await self.bot.fetch_channel(int(logger_id))
+        channel = self.bot.get_channel(int(logger_id))
 
         embed = Embed()
         embed.description = f"Message edited in {before.channel.mention}"
         embed.color = Color.random()
 
-        has_old_content = before.content != ""
-        has_new_content = after.content != ""
+        has_old_content = bool(before.content)
+        has_new_content = bool(after.content)
 
-        old_content = (
-            before.content
-            if len(before.content) < 1024
-            else before.content[:1020] + "..."
-        )
-        new_content = (
-            after.content if len(after.content) < 1024 else after.content[:1020] + "..."
-        )
+        old_content = before.content[:1020] + "..." if len(before.content) >= 1024 else before.content
+        new_content = after.content[:1020] + "..." if len(after.content) >= 1024 else after.content
 
         embed.add_field(
             name="Old message",
@@ -47,59 +43,61 @@ class logger(Cog):
             inline=False,
         )
 
-        embed.set_thumbnail(url=before.author.display_avatar)
+        embed.set_thumbnail(url=before.author.display_avatar.url)
         embed.set_footer(text=f"Author: {before.author} | {before.author.id}")
 
         await channel.send(embed=embed)
 
     @Cog.listener()
     async def on_message_delete(self, message: Message):
-        if not message.author.bot:
-            logger = Logger(message.guild).get_message_logger()
-            if logger == False:
-                return
+        if message.author.bot:
+            return
 
-            try:
-                channel = await self.bot.fetch_channel(int(logger))
-                embed = Embed()
-                embed.description = "Message deleted in {}".format(
-                    message.channel.mention
+        logger_id = Logger(message.guild).get_message_logger()
+
+        if not logger_id:
+            return
+
+        try:
+            channel = self.bot.get_channel(int(logger_id))
+            embed = Embed()
+            embed.description = f"Message deleted in {message.channel.mention}"
+            embed.color = Color.random()
+            attachments = bool(message.attachments)
+            content = bool(message.content)
+
+            if content and not attachments:
+                value = message.content[:1020] + "..." if len(message.content) >= 1024 else message.content
+                embed.add_field(name="Message", value=value, inline=False)
+            elif content and attachments:
+                value = message.content[:1020] + "..." if len(message.content) >= 1024 else message.content
+                embed.add_field(name="Message", value=value, inline=False)
+                embed.set_image(
+                    url=message.attachments[0].url.replace(
+                        "cdn.discordapp.com", "media.discordapp.net"
+                    )
                 )
-                embed.color = Color.random()
-                attachments = bool(message.attachments)
-                content = bool(message.content)
-                if content == True and attachments == False:
-                    if len(message.content) > 1024:
-                        message.content = message.content[:1020] + "..."
-                    embed.add_field(name="Message", value=message.content, inline=False)
-                elif content == True and attachments == True:
-                    if len(message.content) > 1024:
-                        message.content = message.content[:1020] + "..."
-                    embed.add_field(name="Message", value=message.content, inline=False)
-                    embed.set_image(
-                        url=message.attachments[0].url.replace(
-                            "cdn.discordapp.com", "media.discordapp.net"
-                        )
-                    )
-                elif attachments == True and content == False:
-                    embed.add_field(
-                        name="Image",
-                        value="No messages, only media. If you can't see anything, it was a video file",
-                        inline=False,
-                    )
-                    embed.set_image(
-                        url=message.attachments[0].url.replace(
-                            "cdn.discordapp.com", "media.discordapp.net"
-                        )
-                    )
-                embed.set_thumbnail(url=message.author.display_avatar)
-                embed.set_footer(
-                    text="Author: {} | {}".format(message.author, message.author.id)
+            elif attachments and not content:
+                embed.add_field(
+                    name="Image",
+                    value="No messages, only media. If you can't see anything, it was a video file",
+                    inline=False,
                 )
-                await channel.send(embed=embed)
-            except AttributeError:
-                return
+                embed.set_image(
+                    url=message.attachments[0].url.replace(
+                        "cdn.discordapp.com", "media.discordapp.net"
+                    )
+                )
+
+            embed.set_thumbnail(url=message.author.display_avatar.url)
+            embed.set_footer(
+                text=f"Author: {message.author} | {message.author.id}"
+            )
+
+            await channel.send(embed=embed)
+        except AttributeError:
+            return
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(logger(bot))
+    await bot.add_cog(LoggerCog(bot))
