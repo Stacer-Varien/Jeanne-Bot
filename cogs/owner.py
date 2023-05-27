fc7ffbefceb595f34a679d433db627ccf5a7ce16
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-27 12:33:18 +0200

    Redid the owner cog

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,33 +14,32 @@ from discord import (
     Game,
     Activity,
     Object,
-    SyncWebhook,
     HTTPException,
+    SyncWebhook,
 )
 from os import execv
 from sys import executable, argv
 from functions import Botban, Hentai
 from config import BB_WEBHOOK
-from time import time
 from typing import Literal, Optional
 
 
 def restart_bot():
-    execv(executable, ["python"] + argv)
+    execv(executable, [executable] + argv)
 
 
-class slashowner(Cog):
+class OwnerCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @group(aliases=["act", "pressence"], invoke_without_command=True)
+    @group(aliases=["act", "presence"], invoke_without_command=True)
     @is_owner()
     async def activity(self, ctx: Context):
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         embed = Embed(
-            title="This is a group command. However, the available commands for this is:",
+            title="This is a group command. However, the available commands for this are:",
             description="`activity play ACTIVITY`\n`activity listen ACTIVITY`\n`activity clear`",
         )
         await ctx.send(embed=embed)
@@ -49,7 +48,7 @@ class slashowner(Cog):
     @is_owner()
     async def play(self, ctx: Context, *, activity: str):
         """Make Jeanne play something as an activity"""
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         await self.bot.change_presence(activity=Game(name=activity))
@@ -59,7 +58,7 @@ class slashowner(Cog):
     @is_owner()
     async def listen(self, ctx: Context, *, activity: str):
         """Make Jeanne listen to something as an activity"""
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         await self.bot.change_presence(
@@ -71,7 +70,7 @@ class slashowner(Cog):
     @is_owner()
     async def clear(self, ctx: Context):
         """Clears the bot's activity"""
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         await self.bot.change_presence(activity=None)
@@ -82,39 +81,35 @@ class slashowner(Cog):
     async def finduser(self, ctx: Context, user_id: int):
         """Finds a user"""
         await ctx.defer()
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         user = await self.bot.fetch_user(user_id)
-        if user.bot == True:
-            botr = ":o:"
-        else:
-            botr = ":x:"
+        botr = ":o:" if user.bot else ":x:"
         fuser = Embed(title="User Found", color=0xCCFF33)
         fuser.add_field(name="Name", value=user, inline=True)
         fuser.add_field(
             name="Creation Date",
-            value="<t:{}:F>".format(round(user.created_at.timestamp())),
+            value=f"<t:{int(user.created_at.timestamp())}:F>",
             inline=True,
         )
         fuser.add_field(name="Mutuals", value=len(user.mutual_guilds), inline=True)
         fuser.add_field(name="Bot?", value=botr, inline=True)
         fuser.set_image(url=user.display_avatar)
-        if user.banner == None:
+        if user.banner is None:
             await ctx.send(embed=fuser)
         else:
             userbanner = Embed(title="User Banner", color=0xCCFF33)
             userbanner.set_image(url=user.banner)
 
-            e = [fuser, userbanner]
-            await ctx.send(embeds=e)
+            await ctx.send(embeds=[fuser, userbanner])
 
     @command(aliases=["restart", "refresh"])
     @is_owner()
     async def update(self, ctx: Context):
         """Restart me so I can be updated"""
         await ctx.defer()
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
 
         await ctx.send(f"YAY! NEW UPDATE!")
@@ -124,36 +119,37 @@ class slashowner(Cog):
     @is_owner()
     async def botban(self, ctx: Context, user_id: int, *, reason: str):
         """Botban a user from using the bot"""
-        if Botban(ctx.author).check_botbanned_user() == True:
+        if Botban(ctx.author).check_botbanned_user():
             return
-        if reason == None:
+        if not reason:
             await ctx.send("Reason missing for botban", ephemeral=True)
-        else:
-            user = await self.bot.fetch_user(user_id)
-            Botban(user).add_botbanned_user(reason)
+            return
 
-            botbanned = Embed(
-                title="User has been botbanned!",
-                description="They will no longer use Jeanne, permanently!",
-            )
-            botbanned.add_field(name="User", value=user)
-            botbanned.add_field(name="ID", value=user.id, inline=True)
-            botbanned.add_field(name="Reason of ban", value=reason, inline=False)
-            botbanned.set_footer(
-                text="Due to this user botbanned, all data except warnings and softbans are immediatley deleted from the database and banned in the developer's servers! They will have no chance of appealing their botban including their ban and all the commands executed by them are now rendered USELESS!"
-            )
-            botbanned.set_thumbnail(url=user.avatar)
-            webhook = SyncWebhook.from_url(BB_WEBHOOK)
-            webhook.send(embed=botbanned)
+        user = await self.bot.fetch_user(user_id)
+        Botban(user).add_botbanned_user(reason)
 
-            await ctx.send("User botbanned", ephemeral=True)
+        botbanned = Embed(
+            title="User has been botbanned!",
+            description="They will no longer use Jeanne, permanently!",
+        )
+        botbanned.add_field(name="User", value=user)
+        botbanned.add_field(name="ID", value=user.id, inline=True)
+        botbanned.add_field(name="Reason of ban", value=reason, inline=False)
+        botbanned.set_footer(
+            text="Due to this user botbanned, all data except warnings and soft bans are immediately deleted from the database and banned in the developer's servers! They will have no chance of appealing their botban, including their ban, and all the commands executed by them are now rendered USELESS!"
+        )
+        botbanned.set_thumbnail(url=user.avatar)
+        webhook = SyncWebhook.from_url(BB_WEBHOOK)
+        await webhook.send(embed=botbanned)
 
-            orleans = await self.bot.fetch_guild(740584420645535775)
-            ha = await self.bot.fetch_guild(925790259160166460)
-            vhf = await self.bot.fetch_guild(974028573893595146)
+        await ctx.send("User botbanned", ephemeral=True)
 
-            for server in [orleans, ha, vhf]:
-                await server.ban(user, reason=f"Botbanned - {reason}")
+        orleans = await self.bot.fetch_guild(740584420645535775)
+        ha = await self.bot.fetch_guild(925790259160166460)
+        vhf = await self.bot.fetch_guild(974028573893595146)
+
+        for server in [orleans, ha, vhf]:
+            await server.ban(user, reason=f"Botbanned - {reason}")
 
     @command(aliases=["hb", "slice"])
     @is_owner()
@@ -201,4 +197,4 @@ class slashowner(Cog):
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(slashowner(bot))
+    await bot.add_cog(OwnerCog(bot))
