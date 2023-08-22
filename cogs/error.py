e43383559fa30bfff04b3d3a456679d30702a2fc
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-08-23 00:05:56 +0200

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from discord import Color, Embed, Interaction
-from discord import app_commands as Jeanne
+from discord import app_commands as Jeanne, Forbidden
 from discord.ext.commands import Bot, Cog, Context, NotOwner, CommandNotFound
 import traceback
 
@@ -8,29 +8,42 @@ import traceback
 class ErrorsCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.bot.tree.on_error=self.on_app_command_error
+
+    def cog_load(self):
+        tree = self.bot.tree
+        self._old_tree_error = tree.on_error
+        tree.on_error = self.on_app_command_error
+
+    def cog_unload(self):
+        tree = self.bot.tree
+        tree.on_error = self._old_tree_error
 
     @Cog.listener()
     async def on_app_command_error(
-        self, ctx: Interaction, error: Jeanne.errors.AppCommandError
+        self, ctx: Interaction, error: Jeanne.AppCommandError
     ):
-
-        if isinstance(error, Jeanne.errors.MissingPermissions):
+        try:
+            await ctx.response.defer()
+        except:
+            pass
+        if isinstance(error, Jeanne.MissingPermissions):
             embed = Embed(description=str(error), color=Color.red())
             await ctx.followup.send(embed=embed)
+        elif isinstance(error, Jeanne.BotMissingPermissions):
+            embed = Embed(description=str(error), color=Color.red())
+            await ctx.followup.send(embed=embed)            
         elif isinstance(error, Jeanne.errors.CommandInvokeError):
             traceback_error = traceback.format_exception(
                 error, error, error.__traceback__
             )
             with open("errors.txt", "a") as f:
                 f.writelines(f"{datetime.now()} --- {''.join(traceback_error)}")
-        elif isinstance(error, Jeanne.errors.BotMissingPermissions):
             embed = Embed(description=str(error), color=Color.red())
-            await ctx.followup.send(embed=embed)
+            await ctx.followup.send(embed=embed)            
         elif isinstance(error, Jeanne.errors.NoPrivateMessage):
             embed = Embed(description=str(error), color=Color.red())
             await ctx.followup.send(embed=embed)
-        elif isinstance(error, Jeanne.errors.CommandOnCooldown):
+        elif isinstance(error, Jeanne.CommandOnCooldown):
             pass
 
     @Cog.listener()
