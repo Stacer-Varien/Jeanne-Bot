b8c476c8ad9d59fee7206c2d162b7a39881a702b
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-02-12 23:28:31 +0200

    Added new folders in setup_hook

--- jeanne.py
+++ jeanne.py
@@ -6,9 +6,15 @@ from config import TOKEN
 
 class Jeanne(Bot):
     async def setup_hook(self):
-        for filename in listdir("./cogs"):
+        for filename in listdir("./slash_cogs"):
             if filename.endswith(".py"):
-                await self.load_extension(f"cogs.{filename[:-3]}")
+                await self.load_extension(f"slash_cogs.{filename[:-3]}")
+                print(f"{filename} loaded")
+            else:
+                print(f"Unable to load {filename[:-3]}")
+        for filename in listdir("./prefix_cogs"):
+            if filename.endswith(".py"):
+                await self.load_extension(f"prefix_cogs.{filename[:-3]}")
                 print(f"{filename} loaded")
             else:
                 print(f"Unable to load {filename[:-3]}")
@@ -22,10 +28,10 @@ intents.voice_states = False
 intents.auto_moderation = False
 
 bot = Jeanne(
-    command_prefix=when_mentioned_or("J!", "j!", "Jeanne", "jeanne"),
-intents=intents)
+    command_prefix=when_mentioned_or("J!", "j!", "Jeanne", "jeanne"), intents=intents
+)
 bot.allowed_mentions = AllowedMentions.all()
-bot.case_insensitive=True
+bot.case_insensitive = True
 bot.strip_after_prefix = True
 bot.remove_command("help")
 
@@ -34,5 +40,6 @@ bot.remove_command("help")
 async def on_ready():
     print("Connected to bot: {}".format(bot.user.name))
     print("Bot ID: {}".format(bot.user.id))
+
 
 bot.run(TOKEN)
