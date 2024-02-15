71f8d7161c9a94468547bc10b5d49cb4478482ba
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-02-15 16:28:40 +0200

    Created prefixed command logger

---
+++ prefix_cogs/cmd_logger.py
@@ -0,0 +1,19 @@
+from discord.ext.commands import Bot, Cog, Context
+from datetime import datetime
+
+
+class CommandLog(Cog, name="cmdlogger"):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_command_completion(
+        self, ctx: Context
+    ):
+        logged = f"Date and Time = {datetime.now()}\nUser = {ctx.author} | {ctx.author.id}\nCommand used = {ctx.command.qualified_name}\n\n"
+        with open("commandlog.txt", "a") as f:
+            f.writelines(logged)
+
+
+async def setup(bot: Bot):
+    await bot.add_cog(CommandLog(bot))
