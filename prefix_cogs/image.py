09de4e83c329a05365dd5a1307900af01d0c978e
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-02-16 22:59:26 +0200

    Finished the prefixed image commands

--- prefix_cogs/image.py
+++ prefix_cogs/image.py
@@ -1,5 +1,5 @@
 from functions import BetaTest, Botban, Command
-from discord import Color, Embed, Interaction
+from discord import Color, Embed
 from discord.ext.commands import Cog, Bot, Context
 import discord.ext.commands as Jeanne
 from config import kitsune_nekoslife, neko_purrbot
@@ -39,98 +39,174 @@ class images(Cog, name="Images"):
             )
             kitsune.set_image(url=kistune_api["url"])
             await ctx.send(embed=kitsune)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
     @Jeanne.command(description="Need a wallpaper for your PC or phone?")
     async def wallpaper(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.wallpaper.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
-            )
-            return
+        check = await BetaTest(self.bot).check(ctx.author)
 
-        await ctx.defer()
-        embed, file = get_wallpaper_pic()
-        await ctx.send(embed=embed, file=file)
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.wallpaper.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+            embed, file = get_wallpaper_pic()
+            await ctx.send(embed=embed, file=file)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
     @Jeanne.command(description="Get a Jeanne d'Arc image")
     async def jeanne(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.jeanne.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
-            )
-            return
+        check = await BetaTest(self.bot).check(ctx.author)
 
-        await ctx.defer()
-        embed, file = get_jeanne_pic()
-        await ctx.send(embed=embed, file=file)
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.jeanne.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+            embed, file = get_jeanne_pic()
+            await ctx.send(embed=embed, file=file)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
     @Jeanne.command(description="Get a Saber image")
     async def saber(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.saber.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
-            )
-            return
+        check = await BetaTest(self.bot).check(ctx.author)
 
-        await ctx.defer()
-        file, embed = get_saber_pic()
-        await ctx.send(file=file, embed=embed)
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.saber.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+            file, embed = get_saber_pic()
+            await ctx.send(file=file, embed=embed)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
     @Jeanne.command(description="Get a neko image")
     async def neko(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.neko.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
+        check = await BetaTest(self.bot).check(ctx.author)
+
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.neko.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+
+            neko_api = get(neko_purrbot).json()
+            neko = Embed(color=Color.random())
+            neko.set_image(url=neko_api["link"])
+            neko.set_footer(
+                text="Fetched from PurrBot.site • Credits must go to the artist"
             )
+            await ctx.send(embed=neko)
             return
-
-        await ctx.defer()
-
-        neko_api = get(neko_purrbot).json()
-        neko = Embed(color=Color.random())
-        neko.set_image(url=neko_api["link"])
-        neko.set_footer(
-            text="Fetched from PurrBot.site • Credits must go to the artist"
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
         )
-        await ctx.send(embed=neko)
 
     @Jeanne.command(description="Get a Medusa (Fate) image")
     async def medusa(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.medusa.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
-            )
-            return
+        check = await BetaTest(self.bot).check(ctx.author)
 
-        await ctx.defer()
-        embed, file = get_medusa_pic()
-        await ctx.send(embed=embed, file=file)
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.medusa.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+            embed, file = get_medusa_pic()
+            await ctx.send(embed=embed, file=file)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
     @Jeanne.command(description="Get an image from Safebooru")
     async def safebooru(self, ctx: Context):
         if Botban(ctx.author).check_botbanned_user:
             return
-        if Command(ctx.guild).check_disabled(self.safebooru.qualified_name):
-            await ctx.send(
-                "This command is disabled by the server's managers", ephemeral=True
-            )
-            return
+        check = await BetaTest(self.bot).check(ctx.author)
 
-        await ctx.defer()
-        embed = Embed(color=Color.random())
-        embed.set_image(url=safebooru_pic())
-        embed.set_footer(text="Fetched from Safebooru • Credits must go to the artist")
-        await ctx.send(embed=embed)
+        if check == True:
+            if Command(ctx.guild).check_disabled(self.safebooru.qualified_name):
+                await ctx.send(
+                    "This command is disabled by the server's managers", ephemeral=True
+                )
+                return
+
+            await ctx.defer()
+            embed = Embed(color=Color.random())
+            embed.set_image(url=safebooru_pic())
+            embed.set_footer(
+                text="Fetched from Safebooru • Credits must go to the artist"
+            )
+            await ctx.send(embed=embed)
+            return
+        await ctx.send(
+            embed=Embed(
+                description="Uh Oh!\n\nIt seems you are trying something that is meant for beta users.\nIf you wish to join the beta programme, join [Orleans](https://discord.gg/Vfa796yvNq) and ask the bot developer.",
+                color=Color.red(),
+            ),
+            ephemeral=True,
+        )
 
 
 async def setup(bot: Bot):
