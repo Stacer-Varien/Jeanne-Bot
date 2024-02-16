ec02848b773733c7547a48abf87c12cdcc51f993
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-02-16 16:12:26 +0200

    Added prefixed error handler

---
+++ prefix_cogs/error.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from discord import Color, Embed, Interaction
+from discord.ext import commands as Jeanne
+from discord.ext.commands import Bot, Cog, Context, NotOwner, CommandNotFound
+import traceback
+
+
+class ErrorsCog(Cog, name="Errors"):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_command_error(
+        self, ctx: Context, error: Jeanne.CommandError
+    ):
+        if isinstance(error, Jeanne.MissingPermissions):
+            embed = Embed(description=str(error), color=Color.red())
+            await ctx.send(embed=embed)
+        elif isinstance(error, Jeanne.BotMissingPermissions):
+            embed = Embed(description=str(error), color=Color.red())
+            await ctx.send(embed=embed)
+        elif isinstance(error, Jeanne.errors.CommandInvokeError):
+            traceback_error = traceback.format_exception(
+                error, error, error.__traceback__
+            )
+            with open("errors.txt", "a") as f:
+                f.writelines(
+                    f"Date = {datetime.now()}\nComamnd = {ctx.command.qualified_name}\nError:\n{''.join(traceback_error)}\n"
+                )
+        elif isinstance(error, Jeanne.errors.NoPrivateMessage):
+            embed = Embed(description=str(error), color=Color.red())
+            await ctx.send(embed=embed)
+        elif isinstance(error, Jeanne.CommandOnCooldown):
+            pass
+        elif isinstance(error, CommandNotFound):
+            pass
+        elif isinstance(error, NotOwner):
+            pass
+
+
+async def setup(bot: Bot):
+    await bot.add_cog(ErrorsCog(bot))
