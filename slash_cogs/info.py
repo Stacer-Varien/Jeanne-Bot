20afce2819fa76f36c3ed8791b765976b94957e3
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-03-09 23:10:55 +0200

    Update info.py

--- slash_cogs/info.py
+++ slash_cogs/info.py
@@ -52,7 +52,7 @@ class stat_buttons(ui.View):
 class InfoCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.bot_version = "4.4 Beta2"
+        self.bot_version = "4.4 Beta3"
         self.userinfo_context = Jeanne.ContextMenu(
             name="Userinfo", callback=self.userinfo_callback
         )
