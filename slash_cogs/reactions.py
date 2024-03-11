0d9c8e657ad0c282474e99e69a08989939694fdb
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-03-11 23:19:56 +0200

    Fixed reaction commands

--- slash_cogs/reactions.py
+++ slash_cogs/reactions.py
@@ -1,22 +1,24 @@
+import json
+import random
 from functions import Botban, Command
 from discord import Color, Embed, Interaction, Member, app_commands as Jeanne
 from discord.ext.commands import Cog, Bot
 from requests import get
 from config import (
-    hug_nekoslife,
-    slap_nekoslife,
-    smug_nekoslife,
-    poke_nekosfun,
-    cry_purrbot,
-    pat_nekoslife,
-    kiss_nekosfun,
-    tickle_nekoslife,
-    baka_nekosfun,
-    feed_nekoslife,
-    bite_purrbot,
-    blush_purrbot,
-    cuddle_purrbot,
-    dance_purrbot,
+    hug,
+    slap,
+    smug,
+    poke,
+    cry,
+    pat,
+    kiss,
+    tickle,
+    baka,
+    feed,
+    bite,
+    blush,
+    cuddle,
+    dance,
 )
 from typing import Optional
 
@@ -40,22 +42,28 @@ class SlashReactions(Cog):
             )
             return
 
-        reaction_api = get(api_url).json()
+        reaction_api = get(api_url)
         reaction_embed = Embed(color=Color.random())
         reaction_embed.set_footer(
             text=(
-                "Fetched from nekos.life"
-                if "nekos.life" in api_url
-                else "Fetched from PurrBot.site"
+                "Fetched from otakugifs.xyz"
+                if "otakugifs.xyz" in api_url
+                else "Fetched from PurrBot.site" if "purrbot" in api_url else "Fetched from Tenor" if "tenor" in api_url else "Fetched from Nekos.life"
             )
         )
-        reaction_embed.set_image(
-            url=(
-                reaction_api["url"]
-                if "nekos.life" in api_url
-                else reaction_api["image"]
-            )
-        )
+        if "otakugifs.xyz" in api_url:
+            reaction_url = reaction_api.json()["url"]
+        elif "tenor" in api_url:
+            random_gif = random.choice(json.loads(reaction_api.content)["results"])
+            reaction_url = random_gif["media_formats"]["gif"]["url"]
+        else:
+            try:
+                reaction_url = reaction_api.json()["url"]
+            except:
+                reaction_url = reaction_api.json()["link"]
+
+        reaction_embed.set_image(url=reaction_url)
+
         other_actions = ["baka", "smug", "hug"]
         if action == "baka":
             msg = (
@@ -63,20 +71,15 @@ class SlashReactions(Cog):
                 if member is None
                 else f"*{member.mention}*, *{ctx.user} called you a baka!"
             )
-            return
-
-        if action == "smug":
+        elif action == "smug":
             msg = f"*{ctx.user}* is smugging"
-            return
-
-        if action == "hug":
+        elif action == "hug":
             msg = (
                 f"*Hugging {ctx.user}*"
                 if member is None
                 else f"*{ctx.user} hugged {member.mention}*"
             )
-
-        if action not in other_actions:
+        elif action not in other_actions:
             msg = (
                 f"*{action.capitalize()}ing {ctx.user}*"
                 if member is None
@@ -88,69 +91,69 @@ class SlashReactions(Cog):
     @Jeanne.command(description="Hug someone or yourself")
     @Jeanne.describe(member="Who are you hugging?")
     async def hug(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "hug", member, hug_nekoslife)
+        await self._send_reaction(ctx, "hug", member, hug)
 
     @Jeanne.command(description="Slap someone or yourself")
     @Jeanne.describe(member="Who are you slapping?")
     async def slap(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "slap", member, slap_nekoslife)
+        await self._send_reaction(ctx, "slap", member, slap)
 
     @Jeanne.command(description="Show a smuggy look")
     async def smug(self, ctx: Interaction):
-        await self._send_reaction(ctx, "smug", api_url=smug_nekoslife)
+        await self._send_reaction(ctx, "smug", api_url=smug)
 
     @Jeanne.command(description="Poke someone or yourself")
     @Jeanne.describe(member="Who are you poking?")
     async def poke(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "poke", member, poke_nekosfun)
+        await self._send_reaction(ctx, "poke", member, poke)
 
     @Jeanne.command(description="Pat someone or yourself")
     @Jeanne.describe(member="Who are you patting?")
     async def pat(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "pat", member, pat_nekoslife)
+        await self._send_reaction(ctx, "pat", member, pat)
 
     @Jeanne.command(description="Kiss someone or yourself")
     @Jeanne.describe(member="Who are you kissing?")
     async def kiss(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "kiss", member, kiss_nekosfun)
+        await self._send_reaction(ctx, "kiss", member, kiss)
 
     @Jeanne.command(description="Tickle someone or yourself")
     @Jeanne.describe(member="Who are you tickling?")
     async def tickle(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "tickle", member, tickle_nekoslife)
+        await self._send_reaction(ctx, "tickle", member, tickle)
 
     @Jeanne.command(description="Call someone or yourself a baka!")
     @Jeanne.describe(member="Who are you calling a baka?")
     async def baka(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "baka", member, baka_nekosfun)
+        await self._send_reaction(ctx, "baka", member, baka)
 
     @Jeanne.command(description="Feed someone or yourself")
     @Jeanne.describe(member="Who are you feeding?")
     async def feed(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "feed", member, feed_nekoslife)
+        await self._send_reaction(ctx, "feed", member, feed)
 
     @Jeanne.command(description="Make yourself cry")
     async def cry(self, ctx: Interaction):
-        await self._send_reaction(ctx, "cry", api_url=cry_purrbot)
+        await self._send_reaction(ctx, "cry", api_url=cry)
 
     @Jeanne.command(description="Bite someone or yourself")
     @Jeanne.describe(member="Who are you biting?")
     async def bite(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "bite", member, bite_purrbot)
+        await self._send_reaction(ctx, "bite", member, bite)
 
     @Jeanne.command(description="Make yourself blush")
     async def blush(self, ctx: Interaction):
-        await self._send_reaction(ctx, "blush", api_url=blush_purrbot)
+        await self._send_reaction(ctx, "blush", api_url=blush)
 
     @Jeanne.command(description="Cuddle with someone or yourself")
     @Jeanne.describe(member="Who are you cuddling?")
     async def cuddle(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "cuddle", member, cuddle_purrbot)
+        await self._send_reaction(ctx, "cuddle", member, cuddle)
 
     @Jeanne.command(description="Dance with someone or yourself")
     @Jeanne.describe(member="Who are you dancing with?")
     async def dance(self, ctx: Interaction, member: Optional[Member] = None) -> None:
-        await self._send_reaction(ctx, "dance", member, dance_purrbot)
+        await self._send_reaction(ctx, "dance", member, dance)
 
 
 async def setup(bot: Bot):
