30620114dc1ad50506c00bb9f449dd9e5f04ff96
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-04-22 16:50:07 +0200

    Trying to improve the help command

--- prefix/help.py
+++ prefix/help.py
@@ -2,12 +2,13 @@ from json import dumps, loads
 from discord import ButtonStyle, Color, Embed, ui
 from discord.ext.commands import Bot, Cog, group, Context, Range
 import discord.ext.commands as Jeanne
+from reactionmenu import ViewButton, ViewMenu, ViewSelect
 from functions import (
     check_botbanned_prefix,
     check_disabled_prefixed_command,
 )
 from collections import OrderedDict
-from assets.help.modules import modules, Modules
+from assets.help.slash.modules import modules, SlashModules
 
 
 class help_button(ui.View):
@@ -48,25 +49,81 @@ class HelpGroupPrefix(Cog, name="Help"):
     @Jeanne.check(check_disabled_prefixed_command)
     @Jeanne.check(check_botbanned_prefix)
     async def help(self, ctx: Context):
-        await ctx.typing()
-        embed = Embed(
-            color=Color.random(),
-            description="Here is a list of all commands in their modules",
-        )
-        excluded_cogs = ["Jishaku", "Owner"]
-        for cog_name, cog in self.bot.cogs.items():
-            if cog_name not in excluded_cogs:
-                cmds = [
-                    command.qualified_name
-                    for command in cog.walk_commands()
-                    if not command.description.startswith("Main")
-                ]
-                if cmds:
-                    embed.add_field(name=cog_name, value="\n".join(cmds), inline=True)
-        embed.set_footer(
-            text="If you need help on a specific command, please use `help command COMMAND`. COMMAND must be the full command name"
-        )
-        await ctx.send(embed=embed)
+        async with ctx.typing():
+
+            excluded_cogs = [
+                "listenersCog",
+                "tasksCog",
+                "WelcomerCog",
+                "cmdlogger",
+                "ErrorsPrefix",
+                "CommandLogSlash",
+                "Owner",
+                "Jishaku",
+                "guess",
+                "dice",
+                "flip",
+                "CurrencySlash",
+                "ErrorsSlash",
+                "FunSlash",
+                "help",
+                "nsfw",
+                "ImagesSlash",
+                "InfoSlash",
+                "shop",
+                "background",
+                "rank",
+                "levelling",
+                "manage",
+                "create",
+                "edit",
+                "delete",
+                "set",
+                "rename",
+                "command",
+                "level",
+                "moderation",
+                "ReactionsSlash",
+                "embed",
+                "slashutilities",
+                "reminder",
+            ]
+            embed=Embed()
+            embeds=[]
+            menu = ViewMenu(
+                ctx,
+                menu_type=ViewMenu.TypeEmbed,
+                disable_items_on_timeout=True,
+                style="Page $/& | If you need help on a specific command, please use `help command COMMAND`. COMMAND must be the full command name",
+            )
+
+            for i in self.bot.cogs.items():
+                cog_name = i[0]
+                cog = i[1]
+                if cog_name in excluded_cogs:
+                    pass
+                else:
+                    cmds = [
+                        command.qualified_name
+                        for command in cog.walk_commands()
+                        if not command.description.startswith("Main")
+                    ]
+                    embed = Embed(
+                        color=Color.random(),
+                    )
+                    embed.add_field(
+                        name=f"**{cog.qualified_name.title()}**",
+                        value="\n".join(cmds),
+                    )
+                    embeds.append(embed)
+                    menu.add_page(embed=embed)
+
+            menu.add_button(ViewButton.go_to_first_page())
+            menu.add_button(ViewButton.back())
+            menu.add_button(ViewButton.next())
+            menu.add_button(ViewButton.go_to_last_page())
+            menu.add_go_to_select(ViewSelect.GoTo(title="Go to", page_numbers=...))
+            await menu.start()
 
     @help.command(aliases=["cmd"], description="Get help on a certain command")
     @Jeanne.check(check_disabled_prefixed_command)
@@ -139,22 +196,22 @@ class HelpGroupPrefix(Cog, name="Help"):
     @Jeanne.check(check_botbanned_prefix)
     async def module(self, ctx: Context, module: str):
         module_mapping = {
-            "currency": Modules.currency,
-            "fun": Modules.fun,
-            "hentai": Modules.hentai,
-            "image": Modules.image,
-            "pictures": Modules.image,
-            "pics": Modules.image,
-            "images": Modules.image,
-            "info": Modules.info,
-            "information": Modules.info,
-            "levelling": Modules.levelling,
-            "lvl": Modules.levelling,
-            "rank": Modules.levelling,
-            "moderation": Modules.moderation,
-            "mod": Modules.moderation,
-            "reactions": Modules.reactions,
-            "react": Modules.reactions,
+            "currency": SlashModules.currency,
+            "fun": SlashModules.fun,
+            "hentai": SlashModules.hentai,
+            "image": SlashModules.image,
+            "pictures": SlashModules.image,
+            "pics": SlashModules.image,
+            "images": SlashModules.image,
+            "info": SlashModules.info,
+            "information": SlashModules.info,
+            "levelling": SlashModules.levelling,
+            "lvl": SlashModules.levelling,
+            "rank": SlashModules.levelling,
+            "moderation": SlashModules.moderation,
+            "mod": SlashModules.moderation,
+            "reactions": SlashModules.reactions,
+            "react": SlashModules.reactions,
         }
         module_instance = module_mapping.get(module.lower())
         module_data = dumps(modules[module_instance.value])
