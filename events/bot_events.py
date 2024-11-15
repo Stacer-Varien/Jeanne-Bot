779409075f2f6777c98c2fc40703cf1cbd914b8a
unknown <varien.1936@gmail.com>
2024-11-15 23:19:42 +0200

    Renamed shard_events and improved some lines

--- events/bot_events.py
+++ events/bot_events.py
@@ -16,16 +16,19 @@ class ShardEvents(Cog):
             embed.title="A shard is being ratelimited"
             embed.add_field(name="Affected shard", value=shard_id, inline=True)
             embed.add_field(name="Retrying after", value=f"<t:{round(datetime.now()+ timedelta(seconds=60))}:R>", inline=True)
+            embed.set_thumbnail(url="https://files.catbox.moe/0sv1zq.png")
             self.webhook.send(embed=embed)
             await asyncio.sleep(60)
         embed=Embed(color=Color.green())
         embed.description=f"Shard {shard_id} connected successfully!"
+        embed.set_thumbnail(url="https://files.catbox.moe/lerkqk.png")
         self.webhook.send(embed=embed)
 
     @Cog.listener()
     async def on_shard_resumed(self, shard_id:int):
         embed=Embed(color=Color.green())
         embed.description=f"Shard {shard_id} resumed!"
+        embed.set_thumbnail(url="https://files.catbox.moe/lerkqk.png")
         self.webhook.send(embed=embed)
 
     @Cog.listener()
@@ -35,12 +38,20 @@ class ShardEvents(Cog):
             embed.title="A shard is ratelimited"
             embed.add_field(name="Affected shard", value=shard_id, inline=True)
             embed.add_field(name="Retrying after", value=f"<t:{round(datetime.now()+ timedelta(seconds=60))}:R>", inline=True)
+            embed.set_thumbnail(url="https://files.catbox.moe/0sv1zq.png")
             self.webhook.send(embed=embed)
             await asyncio.sleep(60)
         else:
             embed = Embed(color=Color.red())
-            embed.description = f"Shard {shard_id} disconnected and collapsed!"
+            embed.description = f"Shard {shard_id} disconnected!"
+            embed.set_thumbnail(url="https://files.catbox.moe/pkiq8r.png")
             self.webhook.send(embed=embed)
+
+    @Cog.listener()
+    async def on_connect(self):
+        for server in self.bot.guilds:
+            await server.chunk()
+        print("Listening to {} users".format(len(self.bot.users)))
 
 async def setup(bot:Bot):
     await bot.add_cog(ShardEvents(bot))
