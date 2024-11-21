0e4ed0eca02517db4b584bc694095b2f0e7df645
unknown <varien.1936@gmail.com>
2024-11-21 22:08:34 +0200

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -30,25 +30,6 @@ class ErrorsCog(Cog, name="ErrorsSlash"):
         elif isinstance(error, Jeanne.BotMissingPermissions):
             embed = Embed(description=str(error), color=Color.red())
             await ctx.response.send_message(embed=embed)
-        elif isinstance(error, Jeanne.errors.CommandInvokeError):
-            traceback_error = traceback.format_exception(
-                error, error, error.__traceback__
-            )
-            channel = await self.bot.fetch_channel(1257420940128550942)
-            error_message = f"""
-```
-Command: {ctx.command.qualified_name}
-Date and Time: {datetime.now().strftime("%d/%m%Y %H:%M")}
-Error:
-{"".join(traceback_error)}
-```
-"""
-            if len(error_message) > 2000:
-                file = BytesIO(error_message.encode("utf-8"))
-                file = File(fp=file, filename="slash_error.txt")
-                await channel.send(file=file)
-            else:
-                await channel.send(error_message)
         elif isinstance(error, Jeanne.NoPrivateMessage):
             embed = Embed(description=str(error), color=Color.red())
             await ctx.response.send_message(embed=embed)
