6bcfedc3dcac27aa9119091d1f3b23c7652126d8
stacer-varien <jeannebot.discord@gmail.com>
2025-04-16 22:52:31 +0200

    Refactor fun cog: improve code readability, fix typos, and streamline embed usage

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,11 +1,5 @@
 from random import choice, randint
-from discord import (
-    Color,
-    Embed,
-    Interaction,
-    Member,
-    app_commands as Jeanne,
-)
+from discord import Color, Embed, Interaction, Member, app_commands as Jeanne
 from discord.ext.commands import Cog, Bot
 from functions import (
     DevPunishment,
@@ -22,7 +16,7 @@ class fun(Cog, name="FunSlash"):
         self.bot = bot
 
     @Jeanne.command(
-        name="8ball", description="Ask 8 ball anything and you will get your awnser"
+        name="8ball", description="Ask 8 ball anything and you will get your answer"
     )
     @Jeanne.describe(question="Add your question")
     @Jeanne.check(check_botbanned_app_command)
@@ -30,7 +24,7 @@ class fun(Cog, name="FunSlash"):
     @Jeanne.check(is_suspended)
     async def _8ball(self, ctx: Interaction, question: str):
         await ctx.response.defer()
-        eight_ball_answers = [
+        answers = [
             "It is certain.",
             "It is decidedly so.",
             "Without a doubt.",
@@ -57,12 +51,25 @@ class fun(Cog, name="FunSlash"):
             "Yeah... whatever",
             "Yeah... I don't know",
             "Yes? No? I don't know!",
+            "Absolutely not, and I'm offended you asked.",
+            "Sure, if the stars align and pigs fly.",
+            "Only on Tuesdays.",
+            "The answer lies within... your fridge.",
+            "Ask your cat.",
+            "Try again after coffee.",
+            "404 answer not found.",
+            "You're not ready for that truth.",
+            "Do you really want to know?",
+            "Hmm... my magic circuits are glitching.",
+            "I'm just a ball, not a therapist.",
+            "Let me think... nope.",
+            "Yes. But also no.",
+            "If I told you, I'd have to vanish in a puff of smoke.",
         ]
+
         embed = Embed(color=Color.random())
-        embed.add_field(name="Question:", value=f"{question}", inline=False)
-        embed.add_field(
-            name="Answer:", value=f"{choice(eight_ball_answers)}", inline=False
-        )
+        embed.add_field(name="Question:", value=question, inline=False)
+        embed.add_field(name="Answer:", value=choice(answers), inline=False)
         await ctx.followup.send(embed=embed)
 
     @Jeanne.command(description="Say something and I will say it in reversed text")
@@ -72,16 +79,15 @@ class fun(Cog, name="FunSlash"):
     @Jeanne.check(is_suspended)
     async def reverse(self, ctx: Interaction, text: str):
         await ctx.response.defer()
-        filtered_words = ["riffak", "reggin", "aggin"]
-        if any(word in text for word in filtered_words):
+        if any(word in text for word in ["riffak", "reggin", "aggin"]):
             await DevPunishment(ctx.user).add_botbanned_user(
                 "Using the reversed version of a common racial slur"
             )
             return
-        msg = Embed(description=text[::-1], color=Color.random()).set_footer(
-            text="Author: {} | {}".format(ctx.user, ctx.user.id)
+        embed = Embed(description=text[::-1], color=Color.random()).set_footer(
+            text=f"Author: {ctx.user} | {ctx.user.id}"
         )
-        await ctx.followup.send(embed=msg)
+        await ctx.followup.send(embed=embed)
 
     @Jeanne.command(description="Get a random animeme")
     @Jeanne.check(check_botbanned_app_command)
@@ -98,18 +104,17 @@ class fun(Cog, name="FunSlash"):
     @Jeanne.check(check_disabled_app_command)
     async def combine(self, ctx: Interaction, first_word: str, second_word: str):
         await ctx.response.defer()
-        option_name1letters = first_word[: round(len(first_word) / 2)]
-        option_name2letters = second_word[round(len(second_word) / 2) :]
-        option2_name1letters = first_word[round(len(first_word) / 2) :]
-        option2_name2letters = second_word[: round(len(second_word) / 2)]
-        combine1 = "".join([option_name1letters, option_name2letters])
-        combine2 = "".join([option2_name1letters, option2_name2letters])
-        combine = Embed(
-            description=f"**1st combine word**: {combine1}\n**2nd combined word**:{combine2}",
-            color=Color.random(),
+        combine1 = (
+            first_word[: len(first_word) // 2] + second_word[len(second_word) // 2 :]
         )
-        combine.set_author(name=f"{first_word} + {second_word}")
-        await ctx.followup.send(embed=combine)
+        combine2 = (
+            first_word[len(first_word) // 2 :] + second_word[: len(second_word) // 2]
+        )
+        embed = Embed(
+            description=f"**1st combined word**: {combine1}\n**2nd combined word**: {combine2}",
+            color=Color.random(),
+        ).set_author(name=f"{first_word} + {second_word}")
+        await ctx.followup.send(embed=embed)
 
     @Jeanne.command(description="Give me a lot of choices and I will pick one for you")
     @Jeanne.describe(choices="Add your choices here. Separate them with ','")
@@ -118,11 +123,11 @@ class fun(Cog, name="FunSlash"):
     @Jeanne.check(is_suspended)
     async def choose(self, ctx: Interaction, choices: str):
         await ctx.response.defer()
-        choices = choices.split(sep=",")
-        choose = Embed(
-            description=f"I chose **{choice(choices)}**", color=Color.random()
+        embed = Embed(
+            description=f"I chose **{choice(choices.split(','))}**",
+            color=Color.random(),
         )
-        await ctx.followup.send(embed=choose)
+        await ctx.followup.send(embed=embed)
 
     @Jeanne.command(description="Check how much of a simp you are")
     @Jeanne.describe(member="Which member?")
@@ -132,16 +137,15 @@ class fun(Cog, name="FunSlash"):
     async def simprate(self, ctx: Interaction, member: Optional[Member] = None):
         await ctx.response.defer()
         perc = randint(0, 100)
-        member = member if member else ctx.user
-        simp = Embed(
-            description="{}'s simp rate is {}%".format(member, perc),
-            color=Color.random(),
+        member = member or ctx.user
+        embed = Embed(
+            description=f"{member}'s simp rate is {perc}%", color=Color.random()
         )
         if perc >= 75:
-            simp.set_image(url="https://i.imgur.com/W4u4Igk.jpg")
-        elif perc >= 50<75:
-            simp.set_image(url="https://i.imgur.com/Rs1IP2I.jpg")
-        await ctx.followup.send(embed=simp)
+            embed.set_image(url="https://i.imgur.com/W4u4Igk.jpg")
+        elif perc >= 50:
+            embed.set_image(url="https://i.imgur.com/Rs1IP2I.jpg")
+        await ctx.followup.send(embed=embed)
 
     @Jeanne.command(description="Check how gay you are")
     @Jeanne.describe(member="Which member?")
@@ -151,16 +155,15 @@ class fun(Cog, name="FunSlash"):
     async def gayrate(self, ctx: Interaction, member: Optional[Member] = None):
         await ctx.response.defer()
         perc = randint(0, 100)
-        member = member if member else ctx.user
-        gay = Embed(
-            description="{}'s gay rate is {}%".format(member, perc),
-            color=Color.random(),
+        member = member or ctx.user
+        embed = Embed(
+            description=f"{member}'s gay rate is {perc}%", color=Color.random()
         )
         if perc >= 75:
-            gay.set_image(url="https://i.imgur.com/itOD0Da.png?1")
-        elif perc >= 50<75:
-            gay.set_image(url="https://i.imgur.com/tYAbWCl.jpg")
-        await ctx.followup.send(embed=gay)
+            embed.set_image(url="https://i.imgur.com/itOD0Da.png?1")
+        elif perc >= 50:
+            embed.set_image(url="https://i.imgur.com/tYAbWCl.jpg")
+        await ctx.followup.send(embed=embed)
 
 
 async def setup(bot: Bot):
