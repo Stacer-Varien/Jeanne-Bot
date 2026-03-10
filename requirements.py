8d1ce9d4392303326eb2fc15787a852f3a8d14f5
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2026-03-10 12:32:38 +0200

    Redid the requirements.py

--- requirements.py
+++ requirements.py
@@ -1,14 +1,12 @@
 # To run this bot, the packages are set and ready to install.
-import os
 import platform
+import subprocess
 
 if platform.system() == "Windows":  # If you are using a Windows Operating System
     # Upgrades the pip package for fluent installation
-    os.system("python.exe -m pip install -U pip")
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "pip"], check=True)
 elif platform.system() == "Linux":  # If you are using a Linux Operating System
     # Upgrades the pip package for fluent installation #You need Python3.9 and over with an activated venv
-    os.system("python3 -m pip install -U pip")
+    subprocess.run(["python3", "-m", "pip", "install", "--upgrade", "pip"], check=True)
 # Required packages for the bot to use. Also upgrades it if a new release is found
-os.system(
-    "pip install -U discord.py aiohttp websockets requests asyncio python-dotenv humanfriendly datetime Pillow py-expression-eval topggpy reactionmenu jishaku pandas openpyxl openai markdown"
-)
+subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
